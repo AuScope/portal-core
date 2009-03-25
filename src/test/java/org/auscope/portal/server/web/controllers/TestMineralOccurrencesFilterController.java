eb59683ab8cb00095fc1bea516c51b5be8226483
Jarek Sanders <Jarek.Sanders@csiro.au>
2009-03-25 04:54:31 +0000

    Added test for KML converter.

--- src/test/java/org/auscope/portal/server/web/controllers/TestMineralOccurrencesFilterController.java
+++ src/test/java/org/auscope/portal/server/web/controllers/TestMineralOccurrencesFilterController.java
@@ -4,9 +4,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
+import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
+
 
 /**
  * User: Mathew Wyatt
@@ -25,5 +30,45 @@ public class TestMineralOccurrencesFilterController {
         MineralOccurrencesFilterController minOccController = new MineralOccurrencesFilterController();
         minOccController.doMineralOccurrenceFilter("http://www.gsv-tb.dpi.vic.gov.au/AuScope-MineralOccurrence/services?", "Dominion Copper Mine");
     }
+    @Test 
+    public void testConvertToKML() {
+       MineralOccurrencesFilterController minOccController = new MineralOccurrencesFilterController();
+       
+       //C:\Projects\AuScope-Portal
+       String basePath = new File("").getAbsolutePath();
+       //C:\Projects\AuScope-Portal\src\main\webapp\WEB-INF\xsl\ma.xml    mine.xml
+       //String testRunner = "file:///" + basePath + "/src/test/js/testRunner.html";
+       //String testSuite  = new File(new URI("file:///" + basePath.replace("\\", "/") + "/src/main/webapp/WEB-INF/xsl/ma.xml")).getAbsolutePath();
+       //String testSuite1  = new File(new URI("file:///" + basePath.replace("\\", "/") + "/src/main/webapp/WEB-INF/xsl/mine.xml")).getAbsolutePath();
+       String mineFilePath = basePath + "/src/main/webapp/WEB-INF/xsl/mine.xml";
+       String maFilePath = basePath + "/src/main/webapp/WEB-INF/xsl/ma.xml";
+       String s = new String();
+       
+       try {
+          BufferedReader inMine = new BufferedReader (new FileReader(mineFilePath));
+          BufferedReader inMA   = new BufferedReader (new FileReader(maFilePath));
+          try {
+             while ((s = inMine.readLine()) != null )
+                System.out.println(s);
 
+                System.out.println("---------------------");
+                
+             while ((s = inMA.readLine()) != null )
+                System.out.println(s);
+             
+                System.out.println("========---------------------");                
+          } catch(IOException iox) {
+             System.out.println("File read error...");
+             iox.printStackTrace();
+          }
+       
+          System.out.println("....Calling ...minOccController.convertToKML");
+          minOccController.convertToKML(new FileInputStream(mineFilePath), 
+                                        new FileInputStream(maFilePath));
+          
+       } catch (FileNotFoundException e) {
+          System.out.println("File not found...");
+       }
+       
+    }
 }
