b5258e914a15ae7b30876f73cda55a0874b00c7e
Michael Stegherr <Michael.Stegherr@csiro.au>
2009-04-01 09:34:35 +0000

    ADDED: MinOcc test stubs

---
+++ src/test/java/org/auscope/portal/server/web/mineraloccurrence/TestMineralOccurrenceFilter.java
@@ -0,0 +1,32 @@
+package org.auscope.portal.server.web.mineraloccurrence;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.auscope.portal.Util;
+import org.junit.Test;
+
+
+/**
+ * User: Michael Stegherr
+ * Date: 30/03/2009
+ * Time: 3:27:26 PM
+ */
+public class TestMineralOccurrenceFilter {
+
+    @Test
+    public void testMinimumOreAmount() throws IOException {
+        // TODO implementation
+    }
+
+    @Test
+    public void testMinimumCommodityAmount() throws IOException {
+        // TODO implementation
+    }
+
+    @Test
+    public void testCutOffGrade() throws IOException {
+        // TODO implementation
+    }
+}
