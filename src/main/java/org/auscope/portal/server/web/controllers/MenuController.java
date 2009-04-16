ad89d930ad894b64d27a3b34e947169a733abc10
Jarek Sanders <Jarek.Sanders@csiro.au>
2009-04-16 01:30:18 +0000

    Added controller for about.html request mapping

--- src/main/java/org/auscope/portal/server/web/controllers/MenuController.java
+++ src/main/java/org/auscope/portal/server/web/controllers/MenuController.java
@@ -1,7 +1,17 @@
 package org.auscope.portal.server.web.controllers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,7 +27,7 @@ public class MenuController {
    
    protected final Log logger = LogFactory.getLog(getClass());
    
-   /* For the time being we are redirecting Home link to AuScope site
+   /* Commented out, for the time being we are redirecting Home link to AuScope site
    @RequestMapping("/home.html")
    public ModelAndView menu() {
       logger.info("menu controller started!");
@@ -33,5 +43,42 @@ public class MenuController {
    @RequestMapping("/login.html")
    public ModelAndView login() {
       return new ModelAndView("login");
+   }
+   
+   @RequestMapping("/about.html")
+   public ModelAndView about(HttpServletRequest request) {
+
+      String appServerHome = request.getSession().getServletContext().getRealPath("/");
+      File manifestFile = new File(appServerHome,"META-INF/MANIFEST.MF");
+      Manifest mf = new Manifest();
+      ModelAndView mav = new ModelAndView("about");
+      try {
+         mf.read(new FileInputStream(manifestFile));
+         Attributes atts = mf.getMainAttributes();
+         if (mf != null) {            
+            mav.addObject("specificationTitle", atts.getValue("Specification-Title"));
+            mav.addObject("implementationVersion", atts.getValue("Implementation-Version"));
+            mav.addObject("implementationBuild", atts.getValue("Implementation-Build"));
+            mav.addObject("buildDate", atts.getValue("buildDate"));
+            mav.addObject("buildJdk", atts.getValue("Build-Jdk"));
+            mav.addObject("osName", atts.getValue("osName"));
+            mav.addObject("osVersion", atts.getValue("osVersion"));
+            mav.addObject("builtBy", atts.getValue("Built-By"));
+                        
+            mav.addObject("serverName", request.getServerName());
+            mav.addObject("serverInfo", request.getSession().getServletContext().getServerInfo());
+            mav.addObject("serverJavaVersion", System.getProperty("java.version"));
+            mav.addObject("serverJavaVendor", System.getProperty("java.vendor"));
+            mav.addObject("serverOsArch", System.getProperty("os.arch"));
+            mav.addObject("serverOsName", System.getProperty("os.name"));
+            mav.addObject("serverOsVersion", System.getProperty("os.version"));
+         } else {
+            logger.error("Error reading manifest file.");
+         }
+      } catch (IOException e) {
+         /* ignore, since we'll just leave an empty form */
+         e.printStackTrace();
+      }
+      return mav;
    }
 }
