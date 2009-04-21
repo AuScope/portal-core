e50101d845f07cb5024e0e5611f7b16c36ca973d
Jarek Sanders <Jarek.Sanders@csiro.au>
2009-04-21 06:28:50 +0000

    Replaced log4j with apache commons-logging for the sake of consistency.

--- src/main/java/org/auscope/portal/server/util/GmlToKml.java
+++ src/main/java/org/auscope/portal/server/util/GmlToKml.java
@@ -1,22 +1,22 @@
 package org.auscope.portal.server.util;
 
-import org.apache.log4j.Logger;
-
 import java.io.*;
 
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.Transformer;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * <p> This class converts geoSciMl into KML format </p>
  * @author jsanders
  */
 public class GmlToKml {
-   private static final Logger log = Logger.getLogger(GmlToKml.class);
+   private static final Log log = LogFactory.getLog(GmlToKml.class);
    
    /**
     * Utility method to merge xml files.
@@ -25,7 +25,6 @@ public class GmlToKml {
     * @return 
     */   
    public static String convert(String geoXML, InputStream inXSLT) {
-      log.debug("....Convert");
       //log.debug(geoXML);
       
       StringWriter sw = new StringWriter();
