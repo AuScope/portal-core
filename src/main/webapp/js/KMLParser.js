01e593d6bb02238d86760b3e3d46a6a43a31222e
Jarek Sanders <Jarek.Sanders@csiro.au>
2009-10-02 07:32:31 +0000

    Replaced code that uses selectNodes() and selectSingleNode() functions with standard DOM method: getElementsByTagName()
    IE's selectNodes() and selectSingleNode() are of particular uses when manipulating XML and DOM. We have provided XPathFixFirefix.js with code that adds basic XPath parsing functionality to the DOM in FF. However, for some reason those functions do not want to work in IE...

--- src/main/webapp/js/KMLParser.js
+++ src/main/webapp/js/KMLParser.js
@@ -4,22 +4,32 @@ KMLParser = function(kml) {
 };
 
 KMLParser.prototype.makeMarkers = function(icon, markerHandler) {
-    //an array to hold the markers
+    
     var markers = [];
-
-    var placemarks = this.rootNode.selectNodes(".//*[local-name() = 'Placemark']");
-
-    //alert(placemarks[0].selectSingleNode(".//*[local-name() = 'name']").text);
+    // var placemarks = this.rootNode.selectNodes(".//*[local-name() = 'Placemark']");
+    // alert(placemarks[0].selectSingleNode(".//*[local-name() = 'Placemark']").text);
+    
+    var placemarks = this.rootNode.getElementsByTagName("Placemark");
+    
     try {
         for(i = 0; i < placemarks.length; i++) {
-            var name = GXml.value(placemarks[i].selectSingleNode(".//*[local-name() = 'name']"));
-            var description = GXml.value(placemarks[i].selectSingleNode(".//*[local-name() = 'description']"));
-            var coordinates = GXml.value(placemarks[i].selectSingleNode(".//*[local-name() = 'coordinates']")).split(',');
+            // var name = GXml.value(placemarks[i].selectSingleNode(".//*[local-name() = 'name']"));
+            var name = GXml.value(placemarks[i].getElementsByTagName("name")[0]);
+
+            // var description = GXml.value(placemarks[i].selectSingleNode(".//*[local-name() = 'description']"));
+            var description = GXml.value(placemarks[i].getElementsByTagName("description")[0]);
+
+            // var coordinates = GXml.value(placemarks[i].selectSingleNode(".//*[local-name() = 'coordinates']")).split(',');
+            var coordinates = GXml.value(placemarks[i].getElementsByTagName("coordinates")[0]).split(',');
+
+            // We do not want placemarks without coordinates
+            if (coordinates == "")
+                continue;
+            
             //iconlast = GXml.value(placemarks[i].selectSingleNode(".//*[local-name() = 'Icon']/*[local-name() = 'href']")).split(',');
             var lon = coordinates[0];
             var lat = coordinates[1];
             var z = coordinates[2];
-
 
             var point = new GLatLng(parseFloat(lat), parseFloat(lon));
 
