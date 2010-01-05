9e9bd045e3ca8f61698f61ea85a9c546d59769d6
Jarek Sanders <Jarek.Sanders@csiro.au>
2010-01-05 12:05:40 +0000

    NVCL development - scalars plot screen.

--- src/main/webapp/js/geoscimlwfs/nvcl/NvclInfoWindow.js
+++ src/main/webapp/js/geoscimlwfs/nvcl/NvclInfoWindow.js
@@ -29,7 +29,7 @@
     this.wfsUrl = this.Marker.wfsUrl;
     
     /**
-     * Strip pathname from Url and return only protocol + hostname
+     * From Url remove pathname and return only protocol with hostname
      * eg.: http://server.com.au:80/some_path --> http://server.com.au:80 
      */       
     this.wfsServerUrl = (function(url) {
@@ -54,6 +54,9 @@ NvclInfoWindow.prototype = {
         
     },
     
+    /*
+     * Returns datasets for the selected borehole
+     */
     'retrieveDatasets' : function() {
         
         var that = this,
@@ -159,6 +162,7 @@ function showNvclDetails(iServerName, iDatasetId) {
     var endSampleNo = 100;
     var LOG_PATH = '/NVCLDataServices/getLogCollection.html?datasetid=';
     var MOSAIC_PATH = '/NVCLDataServices/mosaic.html?logid=';
+    var PLOT_PATH = '/NVCLDataServices/plotscalar.html?logid=';
 
     alert(iServerName + '\n' + LOG_PATH + '\n' + lDatasetId);
 
@@ -221,7 +225,7 @@ function showNvclDetails(iServerName, iDatasetId) {
         title:  'Borehole Id: ',          
         width:  820,
         //x:    500,
-        //y:    50,
+        y:    50,
         items:[{
             xtype:'tabpanel',
             id   : 'tab-panel',
@@ -234,7 +238,9 @@ function showNvclDetails(iServerName, iDatasetId) {
                 id:      'plottab',
                 iconCls: 'icon-info', 
                 //html: '<div><p><br><b>' + iDatasetId +'<b></p></div>'
-                html: '<iframe style="overflow:auto;width:100%;height:100%;" frameborder="0"  src="' + iServerName + LOG_PATH + lDatasetId + '&startsampleno=' + startSampleNo + logId +'&endsampleno=' + endSampleNo + '"></iframe>' 
+                //html: '<iframe style="overflow:auto;width:100%;height:100%;" frameborder="0"  src="' + iServerName + LOG_PATH + lDatasetId + '&startsampleno=' + startSampleNo + logId +'&endsampleno=' + endSampleNo + '"></iframe>'
+                //html: '<iframe style="overflow:auto;width:100%;height:100%;" frameborder="0"  src="' + iServerName + LOG_PATH + lDatasetId + '"></iframe>'
+                html: 'hello'  
             }]        
         }],             
         
@@ -250,12 +256,213 @@ function showNvclDetails(iServerName, iDatasetId) {
             html: '<iframe style="overflow:auto;width:100%;height:100%;" frameborder="0" src="' + iServerName + MOSAIC_PATH + logId + '&startsampleno=1&endsampleno=100"></iframe>'
        });
     }
+
+    var scalarStore = new Ext.data.XmlStore({
+        url: ProxyURL + iServerName + LOG_PATH + lDatasetId,
+        record: 'Log',
+        idPath: 'LogID',
+        fields: ['LogID','logName']
+    });
+
+
+    
+    var CheckBox = new Ext.grid.CheckboxSelectionModel();
+    var scalarGrid = new Ext.grid.GridPanel({
+        //title: 'Available Scalars',
+        store: scalarStore,
+        sm: CheckBox,
+        columns : [
+            CheckBox,
+        {
+            header: "ID", width: 50, dataIndex: 'LogID', 
+            sortable: true, hidden: true
+        },{
+            id: 'log-name-col', header: "Scalar", width: 100, dataIndex: 'logName',
+            sortable: true
+        }],
+        autoExpandColumn: 'log-name-col',
+        width: 200,
+        height: 450,
+        loadMask: true
+    });
+    
+    scalarStore.load();    
+    // load the store at the latest possible moment
+    /* this.on({
+        afterlayout:{scope:this, single:true, fn:function() {
+            //this.scalarStore.load({params:{start:0, limit:10}});
+            this.scalarStore.load();
+        }}
+    }); */
           
     tab = tp.add({
         title  : 'Scalars',
         id     : 'scalarstab',
         iconCls: 'icon-info',
-        html   : 'Hello'
+        layout : 'fit',
+        border : false,
+        items: {
+            // Bounding form
+            id     :'scalars-form'
+            ,xtype :'form'
+            ,layout:'column'
+            ,frame :true
+             
+            // these are applied to columns
+            ,defaults:{
+                columnWidth:0.5
+                ,layout:'form'
+                ,hideLabels:true
+                ,border:false
+                ,bodyStyle:'padding:10px'
+                ,labelWidth:100
+            }
+            
+            // Columns
+            ,items:[{ // column 1
+                // these are applied to fieldsets
+                defaults:{xtype:'fieldset', layout:'form', anchor:'100%', autoHeight:true}
+                 
+                // fieldsets
+                ,items:[{
+                    title:'List of Scalars'
+                    ,defaultType:'textfield'
+                     
+                    // these are applied to fields
+                    ,defaults:{anchor:'-5', allowBlank:false}
+                     
+                    // fields
+                    ,items:[scalarGrid]
+                }]
+            },{ // column 2
+                // these are applied to fieldsets
+                defaults:{
+                    xtype       :'fieldset',
+                    layout      :'form',
+                    anchor      :'100%',
+                    autoHeight  :true,
+                    paddingRight: '10px'
+                }
+                 
+                // fieldsets
+                ,items:[{
+                    title       :'Hint',
+                    defaultType:'textfield',
+                    // fields
+                    items:[{
+                        xtype  : 'box',
+                        id     : 'indicator',
+                        autoEl : {
+                            tag  : 'div',
+                            html : 'Select a scalar(s) from the "Scalar List" table on the left and then click "Plot" button.<br><br>Leave the default depth values for the entire depth.'
+                        }
+                    }]
+                },{                    
+                    title:'Options',
+                    defaultType:'textfield',
+                 
+                    // these are applied to fields
+                    //,defaults:{anchor:'-20', allowBlank:false}
+                    bodyStyle : 'padding:0 0 0 45px',
+                 
+                    // fields
+                    items:[{
+                        xtype       : 'spinnerfield',
+                        fieldLabel  : 'Start Depth (m)',
+                        name        : 'startdepth',
+                        minValue    : 0,
+                        value       : 0,
+                        accelerate  : true
+                    },{
+                        xtype       : 'spinnerfield',
+                        fieldLabel  : 'End Depth (m)',
+                        name        : 'enddepth',
+                        minValue    : 0,
+                        value       : 99999,
+                        accelerate  : true
+                    },{
+                        xtype       : 'spinnerfield',
+                        fieldLabel  : 'Interval (m)',
+                        name        : 'samplinginterval',
+                        minValue    : 0,
+                        value       : 1.0,
+                        allowDecimals: true,
+                        decimalPrecision: 1,
+                        incrementValue: 0.1,
+                        alternateIncrementValue: 2.1,
+                        accelerate: true                                                                   
+                    }]
+                },{
+                    xtype: 'fieldset',
+                    title       :'Graph Types',
+                    autoHeight: true,
+                    items:[{
+                        xtype  : 'radiogroup',
+                        id: 'ts1',
+                        columns: 1,
+                        items : [
+                            {boxLabel: 'Stacked Bar Chart', name: 'graphtype', inputValue: 1, checked: true},
+                            {boxLabel: 'Scattered Chart', name: 'graphtype', inputValue: 2},
+                            {boxLabel: 'Line Chart', name: 'graphtype', inputValue: 3}
+                        ]
+                    }]                    
+                                                                              
+                }],
+                buttons:[{
+                    text: 'Plot',
+                    handler: function() {
+                        // var urlFull = 'http://nvclwebservices.vm.csiro.au/NVCLDataServices/plotscalar.html
+                        // ?logid=77d0b966-d936-4b9d-bc5c-4b2b12bf1c5
+                        // &startdepth=0
+                        // &enddepth=99999
+                        // &samplinginterval=1
+                        // &width=300
+                        // &height=600
+                        // &graphtype=1';
+
+                        var sHtml = '';
+                        var item_count = scalarGrid.getSelectionModel().getCount();
+                        
+                        //alert(Ext.getCmp('scalars-form').getForm().findField('enddepth').getValue());
+                        //alert(Ext.getCmp('scalars-form').getForm().getValues(true));
+                        var scalarForm = Ext.getCmp('scalars-form').getForm();
+                        
+                        if (item_count > 0) {
+                            var s = scalarGrid.getSelectionModel().getSelections();
+                            for(var i = 0, len = s.length; i < len; i++){
+                                sHtml +='<img src="'; 
+                                sHtml += ProxyURL + iServerName + PLOT_PATH;
+                                sHtml += s[i].data.LogID;
+                                // startDepth=0&endDepth=99999&interval=1&rb-col=2
+                                sHtml += '&' + scalarForm.getValues(true);
+                                sHtml += '&width=300';
+                                sHtml += '&height=600';
+                                sHtml += '"/>';
+                            }
+                        }
+                                                
+                        var winPlot = new Ext.Window({
+                            autoScroll:true,
+                            border: true,        
+                            html: sHtml,
+                            id    : 'plWindow',
+                            layout: 'fit',
+                            maximizable:false,
+                            modal:  true,
+                            plain:  false,
+                            title:  'Plot: ',
+                            autoHeight: true,          
+                            autoWidth: true,
+                            x:10,
+                            y:10,
+                          });                        
+                        winPlot.show();
+                        
+                    }
+                }]
+            }]
+        }
+
     });
     //alert("Before windows show");
     //alert(logId);
@@ -264,6 +471,7 @@ function showNvclDetails(iServerName, iDatasetId) {
     // TO Do: Add check for Plot tab --> Don't display if neither Mosaic nor Plot exist    
     if (logId != -1) {
         win.show();
+
     } else {
         Ext.MessageBox.alert('Info', 'Selected dataset is empty!');
     }
