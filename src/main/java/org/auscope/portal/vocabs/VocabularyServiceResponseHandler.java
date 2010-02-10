9d0770355f987b6fc283129ccb9009c66928080c
Michael Stegherr <Michael.Stegherr@csiro.au>
2010-02-10 05:40:13 +0000

    CHANGED: using getElementsByTagName() instead of XPath

--- src/main/java/org/auscope/portal/vocabs/VocabularyServiceResponseHandler.java
+++ src/main/java/org/auscope/portal/vocabs/VocabularyServiceResponseHandler.java
@@ -8,11 +8,7 @@ import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 
 import org.springframework.stereotype.Repository;
 import org.w3c.dom.Document;
@@ -32,13 +28,8 @@ public class VocabularyServiceResponseHandler {
         domFactory.setNamespaceAware(true); // never forget this!
         DocumentBuilder builder = domFactory.newDocumentBuilder();
         Document rdfDocument = builder.parse(new ByteArrayInputStream(vocabServiceResponse.getBytes("UTF-8")));
-
-        XPathFactory factory = XPathFactory.newInstance();
-        XPath xPath = factory.newXPath();
-        xPath.setNamespaceContext(new VocabularyServiceNamespaceContext());
-
-        XPathExpression expr = xPath.compile("/rdf:RDF/skos:Concept");
-        NodeList conceptNodes = (NodeList)expr.evaluate(rdfDocument, XPathConstants.NODESET);
+        
+        NodeList conceptNodes = rdfDocument.getElementsByTagName("skos:Concept");
         ArrayList<Concept> concepts = new ArrayList<Concept>();
 
         for(int i=0; i < conceptNodes.getLength(); i++) {
