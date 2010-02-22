236c6d45221f18c1ea8f85a24825eceeafeb841e
Jarek Sanders <Jarek.Sanders@csiro.au>
2010-02-22 09:34:54 +0000

    Patch release for the problem where portal is not able to resolve config properties (AUS-1558)

--- src/main/java/org/auscope/portal/server/util/PortalPropertyPlaceholderConfigurer.java
+++ src/main/java/org/auscope/portal/server/util/PortalPropertyPlaceholderConfigurer.java
@@ -18,66 +18,81 @@ import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
  * having to rebuild</p>
  * 
  * @see PropertyPlaceholderConfigurer
+ * @version $Id$
  */
-// HostPlatformPropertyPlaceholderConfigurer
 public class PortalPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
 
-   private static Log log 
-      = LogFactory.getLog(PortalPropertyPlaceholderConfigurer.class);
+    private static Log log 
+            = LogFactory.getLog(PortalPropertyPlaceholderConfigurer.class);
    
-   /**
-    * Resolve the given placeholder
-    * 
-    * @param placeholder - the placeholder to resolve
-    * @return the resolved value, of null if none
-    */
-   public String resolvePlaceholder(String placeholder) {
-      
-      Properties mergedProps;      
-      try {
-         // Merged Properties instance contains both the loaded
-         // properties and properties set on this FactoryBean.
-         mergedProps = mergeProperties();
-      } catch (IOException e) {
-         log.error(e);
-         throw new RuntimeException(e);
-      }
-      
-      return resolvePlaceholder(placeholder, mergedProps);    
+    /**
+     * Resolve the given placeholder
+     * 
+     * @param placeholder - the placeholder to resolve
+     * @return the resolved value, of null if none
+     */
+    public String resolvePlaceholder(String placeholder) {
+        
+        Properties mergedProps;      
+        try {
+            // Merged Properties instance contains both the loaded
+            // properties and properties set on this FactoryBean.
+            mergedProps = mergeProperties();
+        } catch (IOException e) {
+            log.error(e);
+            throw new RuntimeException(e);
+        }
+        
+        return resolvePlaceholder(placeholder, mergedProps);    
+    }
+   
+    /**
+     * Resolve the given placeholder using the given properties.
+     *  
+     * The default implementation checks first for a HOST specific resolution 
+     * request. If this is the case it finds out run time server's hostname/IP
+     * address and prefixes the property name before searching for a mapping 
+     * in configuration file. Otherwise it looks for a property.
+     *  
+     * @param placeholder - the placeholder to resolve
+     * @param props - the merged properties of this configurer
+     * @return the resolved value, of null if none
+     * @see #resolvePlaceholder(String placeholder)
+     */
+    public String resolvePlaceholder(String placeholder, Properties props) {
+        
+        try {
+            if (placeholder.startsWith("HOST.")) {
+                log.info("Local Host: " + InetAddress.getLocalHost().getHostName()
+                                        + " for property " + placeholder);
+                
+                String hostname = InetAddress.getLocalHost().getHostName();
+                
+                // Fix for getHostName() returning FQHN
+                String search = ".";
+                int j = hostname.indexOf(search);
+                if (j!=-1)
+                    hostname = hostname.substring(0,j);
+                
+                log.debug("Host Name: " + hostname);
+                
+                String property = placeholder.replaceFirst("HOST",hostname);
+                
+                log.info("Translated property: " + property);
+                
+                String prop = props.getProperty(property);
+                
+                if (prop == null) 
+                    log.warn("Please define property: " + property);
+                
+                return prop;
+            } else { 
+                return props.getProperty(placeholder);
+            }
+        } catch (UnknownHostException e) {
+            log.warn(e);
+            return null;
+        }
    }
-   
-   /**
-    * Resolve the given placeholder using the given properties.
-    * 
-    * The default implementation checks first for a HOST specific resolution 
-    * request. If this is the case it finds out run time server's hostname/IP
-    * address and prefixes the property name before searching for a mapping 
-    * in configuration file. Otherwise it looks for a property.
-    * 
-    * @param placeholder - the placeholder to resolve
-    * @param props - the merged properties of this configurer
-    * @return the resolved value, of null if none
-    * @see #resolvePlaceholder(String placeholder)
-    */
-   public String resolvePlaceholder(String placeholder, Properties props) {
 
-      try {
-         if (placeholder.startsWith("HOST.")) {
-            log.debug("Host: " + InetAddress.getLocalHost().getHostName()
-                               + " for property " + placeholder);
-            String hostname = placeholder.replaceFirst("HOST",InetAddress.getLocalHost().getHostName());
-            String prop = props.getProperty(hostname);
-            
-            if (prop == null) log.warn("Please define property: " + hostname);
-           
-            return prop;
-         } else { 
-            return props.getProperty(placeholder);
-         }
-      } catch (UnknownHostException e) {
-         log.warn(e);
-         return null;
-      }
-   }
-   
 }
