492ea7d5c762ac57582503de735581c8cae34d8a
Jarek Sanders <Jarek.Sanders@csiro.au>
2010-02-22 09:36:32 +0000

    New package for client related code

---
+++ src/main/java/org/auscope/portal/client/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The classes in this package represent Portal's web client layer.
+ * It should be the home of all code destined to be converted to JavaScript 
+ * for an existence inside browser such as GWT or ZK pre-compiled java code
+ */
+package org.auscope.portal.client;
