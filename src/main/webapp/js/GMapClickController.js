bb08940a26fc08373d63b76312025645dd9d66a2
Terry Rankine <Terry.Rankine@csiro.au>
2010-10-06 16:05:06 +0000

    * WhiteSpace stripping - look up anyedit for eclipse... * No Errors when no data comes back from the geotransects services

--- src/main/webapp/js/GMapClickController.js
+++ src/main/webapp/js/GMapClickController.js
@@ -4,45 +4,45 @@ var genericParserClickHandler = function (map, overlay, latlng, parentOnlineReso
 	if (overlay == null || !overlay.description) {
 		return false;
 	}
-	
+
 	if (!parentOnlineResource) {
 		return false;
 	}
-	
+
 	//The generic parser stamps the description with a specific string followed by the gml:id of the node
 	var genericParserString = 'GENERIC_PARSER:';
-	
+
 	if (overlay.description.indexOf(genericParserString) == 0) {
-		
+
 		//Lets extract the ID and then lookup the parent record
 		//Assumption - We are only interested in the first WFS record
 		var gmlID = overlay.description.substring(genericParserString.length);
 		var wfsUrl = parentOnlineResource.url;
 		var wfsTypeName = parentOnlineResource.name;
-		
+
 		//Parse the parameters to our iframe popup and get that to request the raw gml
 		var html = '<iframe src="genericparser.html';
 		html += '?serviceUrl=' + wfsUrl;
 		html += '&typeName=' + wfsTypeName;
 		html += '&featureId=' + gmlID;
 		html += '" width="600" height="350"/>';
-		
+
 		if (overlay instanceof GMarker) {
 			overlay.openInfoWindowHtml(html);
 		} else {
 			map.openInfoWindowHtml(overlay.getBounds().getCenter(), html);
 		}
-		
+
 		return true;
 	}
-		
+
 	return false;
 };
 
 /**
- * When someone clicks on the google maps we show popups specific to each 
+ * When someone clicks on the google maps we show popups specific to each
  * feature type/marker that is clicked on
- * 
+ *
  * This event is fired when the user clicks on the map with the mouse. A
  * click event passes different arguments based on the context of the
  * click, and whether or not the click occured on a clickable overlay. If
@@ -53,13 +53,13 @@ var genericParserClickHandler = function (map, overlay, latlng, parentOnlineReso
  * the overlay argument contains the overlay object, while the
  * overlaylatlng argument contains the coordinates of the clicked
  * overlay. In addition, a click event is then also fired on the overlay
- * itself. 
- * 
+ * itself.
+ *
  * @param {GMap2}
  * @param {GOverlay} overlay object (such as a GMarker, GPolygon, GPolyline, or GInfoWindow)
  * @param {GLatLng}  geographical coordinates
  * @param {Ext.data.Store}
- * 
+ *
  * @version $Id$
  */
 var gMapClickController = function(map, overlay, latlng, overlayLatlng, activeLayersStore) {
@@ -69,43 +69,43 @@ var gMapClickController = function(map, overlay, latlng, overlayLatlng, activeLa
 	if(reportsMenu) {
 		reportsMenu.destroy();
 	}
-	
+
 	//An instance of ActiveLayersRecord
 	var parentActiveLayerRecord = null;
 	if (overlay && overlay.activeLayerRecord) {
 		parentActiveLayerRecord = new ActiveLayersRecord(overlay.activeLayerRecord);
 	}
-	
+
 	var parentKnownLayer = null;
 	if (parentActiveLayerRecord) {
 		parentKnownLayer = parentActiveLayerRecord.getParentKnownLayer();
 	}
-	
+
 	//an instance of CSWRecord
 	var parentCSWRecord = null;
 	if (overlay && overlay.cswRecord) {
 		parentCSWRecord = new CSWRecord(overlay.cswRecord);
 	}
-	
+
 	//an object as returned from CSWRecord.getOnlineResources()
 	var parentOnlineResource = null;
 	if (overlay) {
 		parentOnlineResource = overlay.onlineResource;
 	}
-	
+
 	//Try to handle a generic parser layer click
 	if (genericParserClickHandler(map,overlay,latlng,parentOnlineResource)) {
 		return;
 	}
-	
+
 	//If a polygon or marker has been clicked, we will have a direct link to the parentActiveLayerRecord
 	if (parentActiveLayerRecord) {
 		//Handle for WFS services (if any)
 	    if (parentKnownLayer && parentKnownLayer.getType() === 'KnownLayerWFS') {
 	    	var wfsTypeName = parentOnlineResource.name;
 	    	var wfsUrl = parentOnlineResource.url;
-	    	
-	    	
+
+
 	    	if (overlay instanceof GMarker) {
 	            if (wfsTypeName === "gsml:Borehole") {
 	                var infoWindow = new NvclInfoWindow(map,overlay, wfsUrl);
@@ -125,36 +125,36 @@ var gMapClickController = function(map, overlay, latlng, overlayLatlng, activeLa
 	        }
 	    	return;
 	    }
-	    
+
 	    //Handle for keyword based groupings of CSW Records
 	    if (parentKnownLayer && parentKnownLayer.getType() === 'KnownLayerKeywords') {
 	    	if (parentKnownLayer.getDescriptiveKeyword() === 'Report') {
 	    		if (overlay instanceof GPolygon) {
-		    
+
 	    			var selectedReports = [];
-	    			
+
 	                for (var i = 0; i < activeLayersStore.getCount(); i++) {
 	                    var alr = activeLayersStore.getActiveLayerAt(i);
-	
+
 	                    //Only consider records that are part of this known layer grouping
-	                    if (alr.getParentKnownLayer() != null && 
+	                    if (alr.getParentKnownLayer() != null &&
 	                    		alr.getParentKnownLayer().getId() == parentKnownLayer.getId()) {
-	                    	
+
 	                    	var overlayManager = alr.getOverlayManager();
-	                    	
+
 	                        for(var j = 0; j < overlayManager.overlayList.length; j++) {
-	                        	
+
 		                        var reportOverlay = overlayManager.overlayList[j];
-		                        
+
 		                        //if this reportOverlay contains the clicked point, add it to the list
 		                        if(reportOverlay.Contains(overlayLatlng)) {
-		                        	
+
 		                        	var cswRecord = new CSWRecord(reportOverlay.cswRecord);
 			                    	var shortTitle = cswRecord.getServiceName();
 		                        	if(shortTitle.length > 40) {
 		                        		shortTitle = shortTitle.substr(0, 60) + "...";
 		                        	}
-		                        	
+
 		                        	selectedReports.push({
 		                				text: shortTitle,
 		                				handler: function(item, checked) {
@@ -168,7 +168,7 @@ var gMapClickController = function(map, overlay, latlng, overlayLatlng, activeLa
 	                    if(selectedReports.length == 1) {
 		                    var infoWindow = new ReportsInfoWindow(map, overlay, parentCSWRecord);
 		                    infoWindow.show();
-	                    } else if(selectedReports.length > 1) { //otherwise show the reports context menu	                        
+	                    } else if(selectedReports.length > 1) { //otherwise show the reports context menu
 
 	                    	//TODO: Shouldn't need this here but for some unknown reason we do...
 	                    	//If the reports popup menu is showing, destroy it.
@@ -176,7 +176,7 @@ var gMapClickController = function(map, overlay, latlng, overlayLatlng, activeLa
 	                    	if(reportsMenu) {
 	                    		reportsMenu.destroy();
 	                    	}
-	                    	
+
 	                    	reportsMenu = new Ext.menu.Menu({
 	                			id: 'reportsMenu',
 	                			style: '',
@@ -184,17 +184,17 @@ var gMapClickController = function(map, overlay, latlng, overlayLatlng, activeLa
 	                			renderTo: 'center_region',
 	                			showSeparator: false
 	                		});
-	                        
+
 	                		var pixel = map.fromLatLngToContainerPixel(overlayLatlng);
 	                		var container = Ext.getCmp('center_region');
-	                        reportsMenu.showAt([container.x + pixel.x, container.y + pixel.y]);	                    	
+	                        reportsMenu.showAt([container.x + pixel.x, container.y + pixel.y]);
 	                    }
 	                }
                 }
 	    	}
 	    	return;
 	    }
-	    
+
 	    //otherwise Handle for WCS services (if any)
 	    cswRecords = parentActiveLayerRecord.getCSWRecordsWithType('WCS');
 	    if (cswRecords.length != 0) {
@@ -212,39 +212,39 @@ var gMapClickController = function(map, overlay, latlng, overlayLatlng, activeLa
     	//We will need to iterate over every WMS to see if they indicate a click event
         for (var i = 0; i < activeLayersStore.getCount(); i++) {
 	    	var alr = new ActiveLayersRecord(activeLayersPanel.getStore().getAt(i));
-	        
+
 	        if (!alr.getLayerVisible()) {
 	        	continue;
 	        }
-	        
+
 	        //each linked WMS record must be tested
 	        var wmsCSWRecords = alr.getCSWRecordsWithType('WMS');
 	        for (var j = 0; j < wmsCSWRecords.length; j++) {
 	        	var wmsOnlineResources = wmsCSWRecords[j].getFilteredOnlineResources('WMS');
-	        	
+
 	        	for (var k = 0; k < wmsOnlineResources.length; k++) {
 	            	map.getDragObject().setDraggableCursor("pointer");
-	                
+
 	                var TileUtl = new Tile(map,latlng);
-	
+
 	                var wmsOnlineResource = wmsOnlineResources[k];
 	                var typeName = wmsOnlineResource.name;
 	                var serviceUrl = wmsOnlineResources[k].url;
-	                
+
 	                var url = "wmsMarkerPopup.do";
 	                url += "?WMS_URL=" + serviceUrl;
 	                url += "&lat=" + latlng.lat();
 	                url += "&lng=" + latlng.lng();
 	                url += "&QUERY_LAYERS=" + typeName;
-	                url += "&x=" + TileUtl.getTilePoint().x; 
+	                url += "&x=" + TileUtl.getTilePoint().x;
 	                url += "&y=" + TileUtl.getTilePoint().y;
 	                url += '&BBOX=' + TileUtl.getTileCoordinates();
 	                url += '&WIDTH=' + TileUtl.getTileWidth();
 	                url += '&HEIGHT=' + TileUtl.getTileHeight();
-	                
+
 	                if(typeName.substring(0, typeName.indexOf(":")) == "gt") {
 	                	handleGeotransectWmsRecord(url, wmsCSWRecords[j], wmsOnlineResource, map, latlng);
-	                } else {    
+	                } else {
 	                	handleGenericWmsRecord(url, i, map, latlng);
 	            	}
 	        	}
@@ -256,24 +256,24 @@ var gMapClickController = function(map, overlay, latlng, overlayLatlng, activeLa
 /**
  * Request json data from url, process response and open a GeotransectsInfoWindow
  * to present the data.
- * 
+ *
  * @param url
  * @param activeLayersStore
  * @param map
  * @param latlng
  */
 function handleGeotransectWmsRecord(url, cswRecord, wmsOnlineResource, map, latlng) {
-	
+
 	url += "&INFO_FORMAT=application/vnd.ogc.gml";
-	
+
     Ext.Ajax.request({
     	url: url,
     	timeout		: 180000,
     	wmsOnlineResource : wmsOnlineResource,
     	cswRecord : cswRecord,
     	success: function(response, options) {
-            if (isGmlDataThere(response.responseText)) {                                  
-            	
+            if (isGmlDataThere(response.responseText)) {
+
             	//Parse the response
 				var XmlDoc = GXml.parse(response.responseText);
 				if (g_IsIE) {
@@ -283,21 +283,21 @@ function handleGeotransectWmsRecord(url, cswRecord, wmsOnlineResource, map, latl
 				if (!rootNode) {
 				  return;
 				}
-				
+
 				var schemaLoc = rootNode.getAttribute("xsi:schemaLocation");
 
-				var reqTypeName = schemaLoc.substring(schemaLoc.indexOf("typeName")+9, 
+				var reqTypeName = schemaLoc.substring(schemaLoc.indexOf("typeName")+9,
             			schemaLoc.indexOf(' ', schemaLoc.indexOf("typeName")+9));
             	//Browser may have replaced certain characters
             	reqTypeName = reqTypeName.replace("%3A", ":");
 
                 //Extract the line Id from the XML
             	var line = rootNode.getElementsByTagName("gt:LINE");
-            	if(line == null || line.length <= 0) { 
+            	if(line == null || line.length <= 0) {
             		//Chrome, Opera may not want the namespace prefix
             		line = rootNode.getElementsByTagName("LINE");
             	}
-            	
+
             	// Change to enable the SURV_LINE which is the key in the shapefile
             	if(line == null || line.length <= 0) {
             		line = rootNode.getElementsByTagName("gt:SURV_LINE");
@@ -305,9 +305,9 @@ function handleGeotransectWmsRecord(url, cswRecord, wmsOnlineResource, map, latl
                 		line = rootNode.getElementsByTagName("SURV_LINE");
                 	}
             	}
-            	
-            	
-            	
+
+
+
                 var lineId = "";
 
                 if(line != null && line.length > 0) {
@@ -320,17 +320,17 @@ function handleGeotransectWmsRecord(url, cswRecord, wmsOnlineResource, map, latl
             	    if(lineId.indexOf("cdp") == 0) {
             	    	lineId = lineId.substring(3, lineId.length);
             	    }
-            	    
+
                 	var infoWindow = new GeotransectsInfoWindow(latlng, map, lineId, options.cswRecord, options.wmsOnlineResource);
                 	infoWindow.show();
                 } else {
                 	alert("Remote server returned an unsupported response.");
                 }
-                         
+
             }
     	},
     	failure: function(response, options) {
-    		Ext.Msg.alert('Error requesting data', 'Error (' + 
+    		Ext.Msg.alert('Error requesting data', 'Error (' +
     				response.status + '): ' + response.statusText);
     	}
     });
@@ -338,7 +338,7 @@ function handleGeotransectWmsRecord(url, cswRecord, wmsOnlineResource, map, latl
 }
 /**
  * Request html data from the url and open an info window to present the data.
- * 
+ *
  * @param url
  * @param map
  * @param latlng
@@ -346,19 +346,19 @@ function handleGeotransectWmsRecord(url, cswRecord, wmsOnlineResource, map, latl
 function handleGenericWmsRecord(url, i, map, latlng) {
 
  	url += "&INFO_FORMAT=text/html";
- 	
+
     GDownloadUrl(url, function(response, responseCode) {
         if (responseCode == 200) {
             if (isHtmlDataThere(response)) {
-                if (isHtmlPage(response)) {                           	                  	
+                if (isHtmlPage(response)) {
                     var openWindow = window.open('','mywindow'+i);
                     if (openWindow) {
                         openWindow.document.write(response);
                         openWindow.document.close();
                     } else {
                     	alert('Couldn\'t open popup window containing WMS information. Please disable any popup blockers and try again');
-                    }                                                     	
-                } else {                           	                        		
+                    }
+                } else {
                 	map.openInfoWindowHtml(latlng, response, {autoScroll:true});
                 }
             }
@@ -375,10 +375,10 @@ function handleGenericWmsRecord(url, i, map, latlng) {
 }
 
 /**
- * Returns true if the WMS GetFeatureInfo query returns valid gml data 
- * describing a feature. Verifies this by ensuring the gml contains at least 
+ * Returns true if the WMS GetFeatureInfo query returns valid gml data
+ * describing a feature. Verifies this by ensuring the gml contains at least
  * 1 featureMember.
- * 
+ *
  * @param iStr GML string content to be verified
  * @return true if the WMS GetFeatureInfo query returns valid gml data.
  */
@@ -389,25 +389,25 @@ function isGmlDataThere(iStr) {
 
 /**
  * Returns true if WMS GetFeatureInfo query returns data.
- * 
+ *
  * We need to hack a bit here as there is not much that we can check for.
  * For example the data does not have to come in tabular format.
  * In addition html does not have to be well formed.
  * In addition an "empty" click can still send style information
- * 
+ *
  * So ... we will assume that minimum html must be longer then 30 chars
  * eg. data string: <table border="1"></table>
- * 
+ *
  * For a bit of safety lets only count the bytes in the body tag
- * 
- * @param {iStr} HTML string content to be verified 
+ *
+ * @param {iStr} HTML string content to be verified
  * @return {Boolean} Status of the
  */
-function isHtmlDataThere(iStr) {	
+function isHtmlDataThere(iStr) {
 	//This isn't perfect and can technically fail
 	//but it is "good enough" unless you want to start going mental with the checking
 	var lowerCase = iStr.toLowerCase();
-	
+
 	//If we have something resembling well formed HTML,
 	//We can test for the amount of data between the body tags
 	var startIndex = lowerCase.indexOf('<body>');
@@ -415,8 +415,8 @@ function isHtmlDataThere(iStr) {
 	if (startIndex >= 0 || endIndex >= 0) {
 		return ((endIndex - startIndex) > 32);
 	}
-		
-	//otherwise it's likely we've just been sent the contents of the body 
+
+	//otherwise it's likely we've just been sent the contents of the body
 	return lowerCase.length > 32;
 }
 
--- src/main/webapp/js/wms/GeotransectsInfoWindow.js
+++ src/main/webapp/js/wms/GeotransectsInfoWindow.js
@@ -1,95 +1,95 @@
 /**
  * @class GeotransectsInfoWindow
- * 
+ *
  * @author jac24m
  */
 
 /**
  * cswRecord - CSWRecord - the CSW Record that is a parent to onlineResource
- * onlineResource - Object - The online resource that was used to render this geotransect 
+ * onlineResource - Object - The online resource that was used to render this geotransect
  * @constructor
  */
 function GeotransectsInfoWindow(iLatlng, iMap, iLineId, iCSWRecord, iOnlineResource) {
-    this.map = iMap; 
+    this.map = iMap;
     this.latlng = iLatlng;
     this.cswRecord = iCSWRecord;
     this.onlineResource = iOnlineResource;
-    this.tabsArray = [];   
-    
+    this.tabsArray = [];
+
     this.lineId = iLineId;
     this.description = this.cswRecord.getDataIdentificationAbstract();
-    
-    //Get the GeoServer's service Url, removing pathname and return only protocol with 
-    //hostname eg.: http://server.com.au/nvcl/wfs --> http://server.com.au      
-    this.geoServerUrl = (function(url) {        
-        var str = url.slice( ("http://").length);   
+
+    //Get the GeoServer's service Url, removing pathname and return only protocol with
+    //hostname eg.: http://server.com.au/nvcl/wfs --> http://server.com.au
+    this.geoServerUrl = (function(url) {
+        var str = url.slice( ("http://").length);
         return 'http://' + str.slice(0,str.indexOf("/"));
-    })(this.onlineResource.url);  
-    
+    })(this.onlineResource.url);
+
     this.mask = new Ext.LoadMask(Ext.get('center_region'), {msg:"Please wait..."});
 }
 
 GeotransectsInfoWindow.prototype = {
-		
+
     'TAB_1' : "Summary",
     'TAB_2' : "Seismic Data",
     'TAB_3' : "SegY Data",
-    
+
     'GETSEISMICSECTIONS' : "/geotransect-dataservices/getSeismicSections.json?",
-    
+
     'GETSEGYDATASETS' : "/geotransect-dataservices/getSEGYDatasets.json?",
-	    
+
 	'show': function() {
 		//Open our window with the basic info displayed
-		this.tabsArray[0] = new GInfoWindowTab(this.TAB_1, 
+		this.tabsArray[0] = new GInfoWindowTab(this.TAB_1,
 				"<div style='min-width:400; min-height:300;'>" +
-				"<table border=\"1\" cellspacing=\"1\" width=\"100%\" bgcolor=\"#EAF0F8\">" 
+				"<table border=\"1\" cellspacing=\"1\" width=\"100%\" bgcolor=\"#EAF0F8\">"
 				+"<tr><td><pre style=\"white-space:pre-wrap;white-space:-moz-pre-wrap;" +
 						"white-space:-pre-wrap;white-space:-o-pre-wrap;word-wrap:break-word;" +
-						"width:99%;overflow:auto;\">" 
-				+ this.description 
-				+ "</pre></td></tr>" 
+						"width:99%;overflow:auto;\">"
+				+ this.description
+				+ "</pre></td></tr>"
 				+"</table>" +
 			    "</div>");
-		
-		//The following initialisation is required as the tabs later added asynchronously 
+
+		//The following initialisation is required as the tabs later added asynchronously
 		//may end up being added in the wrong order (2 before 1 exists) resulting in errors
 		//such as: contextElem is null or not an object.
 		this.tabsArray[1] = new GInfoWindowTab(this.TAB_2, "");
 		this.tabsArray[2] = new GInfoWindowTab(this.TAB_3, "");
-		
-		var me = this;		
 
-        this.map.openInfoWindowTabs(this.latlng, this.tabsArray, 
+		var me = this;
+
+        this.map.openInfoWindowTabs(this.latlng, this.tabsArray,
         		{maxWidth:800, maxHeight:300, autoScroll:true,
         	onOpenFn:function(){
-                me.retrieveDatasets(); 
+                me.retrieveDatasets();
         	}});
 
 	},
 
 	'retrieveDatasets' : function() {
-	
+
 		this.map.getInfoWindow().hide();
-        this.mask.show();  
-        
+        this.mask.show();
+
 		//request data urls - seismic section data
-	    var url = this.geoServerUrl;    
+	    var url = this.geoServerUrl;
         url += this.GETSEISMICSECTIONS;
         url += "lineId="+this.lineId;
         this.requestData(url, 1, this.TAB_2);
 
-		//request data urls - segy data  
+		//request data urls - segy data
 	    url = this.geoServerUrl;
     	url += this.GETSEGYDATASETS;
         url += "lineId="+this.lineId;
         this.requestData(url, 2, this.TAB_3);
-                
+
 	},
-	
+
 	'requestData' : function(url, tabI, label) {
-		var me = this;   
-		
+		var me = this;
+
         Ext.Ajax.request({
         	url: 'requestGeotransectsData.do',
         	timeout		: 180000,
@@ -98,41 +98,42 @@ GeotransectsInfoWindow.prototype = {
         	},
         	success: function(response, options) {
         		var responseObj = Ext.util.JSON.decode(Ext.util.JSON.decode(response.responseText).json);
-       			
+
         		//Generate an error / success fragment to display to the user
-                if (!responseObj.result.success) {                  
-                    this.mask.hide();  
+        		if (!responseObj.result.success) {
+//              	this.mask.hide();
         			Ext.Msg.alert('Error downloading data', 'There was an error whilst communicating with ' + url);
         			return;
-        		} else if (responseObj.items.length == 0) {       	        
-        	        this.mask.hide();  
-        			Ext.Msg.alert('Error downloading data', 'The URL ' + url + ' returned no parsable records');
-        			return;
         		}
-                        		              
+//        		} else if (responseObj.items.length == 0) {
+//        	        this.mask.hide();
+//        			Ext.Msg.alert('Error downloading data', 'The URL ' + url + ' returned no parsable records');
+//        			return;
+//        		}
+
         		//Parse records and download the data
-                var values = [responseObj.items.length];               
+                var values = [responseObj.items.length];
             	for (var i = 0; i < responseObj.items.length; i++) {
             		values[i] = responseObj.items[i].url;
             	}
-            	
-            	me.addDataTab(values, tabI, label);     
-            	
+
+            	me.addDataTab(values, tabI, label);
+
         	},
-        	failure: function(response, options) {   
-                this.mask.hide();  
+        	failure: function(response, options) {
+//                this.mask.hide();
         		Ext.Msg.alert('Error requesting data', 'Error (' + response.status + '): ' + response.statusText);
         	}
         });
 	},
-	
+
 	'addDataTab' : function(values, tabI, label) {
 		//Parse records and build html string
     	var sHtml = "";
-    	
-		if(values.length > 0) {             
-			sHtml += '<div style="padding-bottom:10px;" >' + 
-				'Available data sets:' + 
+
+		if(values.length > 1) {
+			sHtml += '<div style="padding-bottom:10px;" >' +
+				'Available data sets:' +
 				'</div>' +
 				'<div>' +
 				'Note: Browsers may not be able to display the following data due to the '+
@@ -141,25 +142,35 @@ GeotransectsInfoWindow.prototype = {
 				'</div>' +
 				'<div>' +
 				"<table border=\"1\" cellspacing=\"1\" width=\"100%\" bgcolor=\"#EAF0F8\">";
-        
+
 			for (var i = 0; i < values.length; i++) {
 				sHtml += "<tr><td>";
-				sHtml += "<a href='" + values[i] + "' target='_blank'>"+ values[i] +"</a><br/>";	
+				sHtml += "<a href='" + values[i] + "' target='_blank'>"+ values[i] +"</a><br/>";
 				sHtml += "</tr></td>";
-			}                          	
+			}
 
 			sHtml += "</table>";
-			sHtml += '</div>'; 
+			sHtml += '</div>';
+	        this.mask.hide();
 		}
-    	
-    	//Create new tab
+		else {
+			sHtml += '<div style="padding-bottom:10px;" >' +
+			'No DataSets available.' +
+			'</div>'
+	        this.mask.hide();
+		}
+
+		//Create new tab
         this.tabsArray[tabI] = new GInfoWindowTab(label, sHtml);
-		
+
         //Add new tab to pop-up window
-        this.map.updateInfoWindow(this.tabsArray);  
-        
-        this.mask.hide();  
-		this.map.getInfoWindow().show();
+        this.map.updateInfoWindow(this.tabsArray);
+
+
+
+//        if(values.length > 1) {
+//        	this.map.getInfoWindow().show();
+//        }
 	}
-	
+
 };
