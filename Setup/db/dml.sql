e8e771a5914b62f1edd9da6bc8c7ea6429ebb00e
Terry Rankine <Terry.Rankine@csiro.au>
2010-10-12 15:12:38 +0000

    AUS-1929 - Removed unused database dependency. Deleted old JSP files.
    Added Tania's machine to config.properties

--- Setup/db/dml.sql
+++
@@ -1,4 +0,0 @@
-insert into users values ('Jarek.Sanders@csiro.au','jarekspassword',true);
-insert into authorities values ('Jarek.Sanders@csiro.au','ROLE_ADMINISTRATOR');
-insert into authorities values ('Jarek.Sanders@csiro.au','ROLE_DOWNLOAD');
-
--- src/main/webapp/WEB-INF/jsp/hello.jsp
+++
@@ -1,7 +0,0 @@
-<html>
-  <head><title>Hello :: Spring Application</title></head>
-  <body>
-    <h1>Hello - Spring Application</h1>
-    <p>Greetings.</p>
-  </body>
-</html>
