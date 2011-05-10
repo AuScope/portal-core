d0f4cb207e0a45d741d8e4e5152fce9a136e183c
Josh Vote <Josh.Vote@csiro.au>
2011-05-10 06:18:21 +0000

    AUS-2058 - GUI support for pressure db popups

--- src/main/webapp/js/wfs/PressureDbInfoWindow.js
+++ src/main/webapp/js/wfs/PressureDbInfoWindow.js
@@ -11,8 +11,8 @@
  */
 function PressureDbInfoWindow(iMap, iMarker, wfsUrl) {
 
-    this.Map = iMap;
-    this.Marker = iMarker;
+    this.map = iMap;
+    this.marker = iMarker;
     this.boreholeId = iMarker.title || "";
     this.waitHtml =
         '<div>' +
@@ -21,24 +21,150 @@ function PressureDbInfoWindow(iMap, iMarker, wfsUrl) {
                 '<img src="img/wait.gif" style="padding-top:50px;" />' +
             '</p>' +
         '</div>';
-    this.wfsServiceUrl = wfsUrl;
+    this.wfsUrl = wfsUrl;
 
     /**
      * iMarker.wfsUrl comes from GeoNetwork and represents GeoServer's service
      * Url. From this Url we remove pathname and return only protocol with
      * hostname eg.: http://server.com.au/nvcl/wfs --> http://server.com.au
      */
-    this.geoServerUrl = (function(url) {
-        var str = url.slice( ("http://").length);
-        return 'http://' + str.slice(0,str.indexOf("/"));
-    })(wfsUrl);
+    var str = wfsUrl.slice( ("http://").length);
+    this.baseUrl = 'http://' + str.slice(0,str.indexOf("/"));
+    this.serviceUrl = this.baseUrl + '/pressuredb-dataservices'; //This is a hack - somehow this needs to make it to the registry
 }
 
 PressureDbInfoWindow.prototype = {
 
+    //Private fields
+    'CONTROLLER_GET_AVAILABLE_OM' : 'pressuredb-getAvailableOM.do',
+    'CONTROLLER_DOWNLOAD' : 'pressuredb-download.do',
+    'marker' : null,
+    'map' : null,
+    'waitHtml' : '',
+    'boreholeId' : '',
+    'wfsUrl' : '',
+    'baseUrl' : '',
+    'serviceUrl' : '',
+    'tabList' : [],
+    
+    //Downloads the specified URl in a new window for the user to actually save to disk
+    'downloadUrl' : function(url) {
+        var body = Ext.getBody();
+        var frame = body.createChild({
+            tag:'iframe',
+            //cls:'x-hidden',
+            id:'iframe',
+            name:'iframe'
+        });
+        var form = body.createChild({
+            tag:'form',
+            //cls:'x-hidden',
+            id:'form',
+            target:'iframe',
+            method:'POST'
+        });
+        form.dom.action = url;
+        form.dom.submit();
+    },
+    
+    //Creates the extjs content of the OM tab 
+    'renderAvailableOMTab' : function(divId, availableOmResponse) {
+        var me = this;
+        var panel = new Ext.form.FormPanel({
+           renderTo : divId,
+           width : 300,
+           autoHeight : true,
+           items : [{
+               xtype : 'checkboxgroup',
+               fieldLabel: 'Temperature',
+               items : [{
+                   boxLabel : 'T',
+                   name : 't',
+                   disabled : availableOmResponse.temperatureT
+               }]
+           },{
+               xtype : 'checkboxgroup',
+               fieldLabel: 'Salinity',
+               items : [{
+                   boxLabel : 'TDS',
+                   name : 'tds',
+                   disabled : availableOmResponse.salinityTds
+               },{
+                   boxLabel : 'NaCl',
+                   name : 'nacl',
+                   disabled : availableOmResponse.salinityNacl
+               },{
+                   boxLabel : 'Cl',
+                   name : 'cl',
+                   disabled : availableOmResponse.salinityCl
+               }]
+           },{
+               xtype : 'checkboxgroup',
+               fieldLabel: 'Pressure',
+               items : [{
+                   boxLabel : 'RFT',
+                   name : 'rft',
+                   disabled : availableOmResponse.pressureRft
+               },{
+                   boxLabel : 'DST',
+                   name : 'dst',
+                   disabled : availableOmResponse.pressureDst
+               },{
+                   boxLabel : 'FITP',
+                   name : 'fitp',
+                   disabled : availableOmResponse.pressureFitp
+               }]
+           }],
+           buttons : [{
+               xtype : 'button',
+               text : 'Download',
+               handler : function() {
+                   //We need to generate our download URL
+                   var url = Ext.urlAppend(me.CONTROLLER_DOWNLOAD, Ext.urlEncode({wellID : me.boreholeId}));
+                   url = Ext.urlAppend(url, Ext.urlEncode({serviceUrl : me.serviceUrl}));
+                   
+                   //Find the parent form - iterate the selected values
+                   var parentForm = this.findParentByType('form');
+                   for (feature in parentForm.getForm().getValues()) {
+                       url = Ext.urlAppend(url, Ext.urlEncode({feature : feature}));
+                   }
+                   
+                   me.downloadUrl(url);
+               }
+           }]
+        });
+    },
+    
+    //Makes a request to the pressure DB dataservice for the list of available datasets for this borehole
+    'retrieveAvailableOM' : function() {
+        Ext.Ajax.request({
+            url : this.CONTROLLER_GET_AVAILABLE_OM,
+            params : {
+                wellID : this.boreholeId,
+                serviceUrl : this.serviceUrl
+            },
+            scope : this,
+            success : function(response) {
+                var responseObj = Ext.util.JSON.decode(response.responseText);
+                if (responseObj && responseObj.success) {
+                    //This is our html page - our ext js panel will render to the div here
+                    var divId = 'pressuredb-om';
+                    var baseHtml = '<html><body><div id="' + divId + '"/></body></html>';
+                    
+                    //So update the page according to what we get from the data service
+                    this.tabList.push(new GInfoWindowTab("Available OM", baseHtml));
+                    this.map.updateInfoWindow(this.tabList);
+                    this.renderAvailableOMTab(divId, responseObj.data[0]);
+                }
+            }
+        });
+    },
+    
+    //Shows this info window opened up on the marker used in the constructor
     'show': function() {
-    	var indexOfDes = this.Marker.description.indexOf('<');
-    	var overlayDescription =this.Marker.description.substring(indexOfDes);
+        //Extract the transformed HTML snippet into a proper HTML 'page' ready for display on the first tab
+    	var indexOfDes = this.marker.description.indexOf('<');
+    	var overlayDescription =this.marker.description.substring(indexOfDes);
     	var summaryHtml ='';
     	summaryHtml += '<html>';
     	summaryHtml += '<body>';
@@ -53,65 +179,17 @@ PressureDbInfoWindow.prototype = {
     	summaryHtml += overlayDescription;
     	summaryHtml += '</div>';
     	
+    	this.tabList = [];
+    	this.tabList.push(new GInfoWindowTab("Summary", summaryHtml));
+
         var me = this;
-        var serverAddr = this.geoServerUrl;
-        
-        summaryHtml += '<div align="right">' +
-        						'<br/>' +
-        			              '<input type="button" id="downloadDatasetBtn" value="Download Observations" name=butDownloadDataset onclick="openPressureDbDownloadWindow(\''+ this.boreholeId +'\',\''+ this.wfsServiceUrl +'\',\''+ this.geoServerUrl +'\')">';
-        summaryHtml +=	'</div>';
-        summaryHtml += '</body>';
-        summaryHtml += '</html>';
-    	
-    	this.Marker.openInfoWindowHtml(summaryHtml);
+        this.marker.openInfoWindowTabs(this.tabList, {
+            onOpenFn:function(){
+                //And update it with the downloaded data as it arrives
+                me.retrieveAvailableOM();
+            }
+        });
     }
 
 }; // End of PressureDbInfoWindow.prototype
-
-var openPressureDbDownloadWindow = function(boreholeId, wfsServiceUrl, geoServerUrl)
-{
-	var keys = [];
-    var values = [];
-    
-    request = wfsServiceUrl;
-    request += '?request=getFeature';
-    request += '&typeName=sa:SamplingFeatureCollection';
-    request += '&featureId=' + 'sa.samplingfeaturecollection.' + boreholeId;
-    
-    keys.push("serviceUrls");
-    values.push(request);
-    
-	keys.push("filename");
-	values.push("PressureDb_Download");
-    
-    url = "downloadDataAsZip.do?";
-    
-    if (keys && values && (keys.length == values.length)) {
-        for (var i = 0; i < keys.length; i++) {
-            url += '&' + keys[i] + '=' + escape(values[i]);
-        }
-    }
-
-    download(url);
-};
-
-//downloads given specified file.
-download = function(url) {
-    var body = Ext.getBody();
-    var frame = body.createChild({
-        tag:'iframe',
-        cls:'x-hidden',
-        id:'iframe',
-        name:'iframe'
-    });
-    var form = body.createChild({
-        tag:'form',
-        cls:'x-hidden',
-        id:'form',
-        target:'iframe',
-        method:'POST'
-    });
-    form.dom.action = url;
-    form.dom.submit();
-};
 
