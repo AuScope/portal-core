cbc9ae3d144a216df7810069bf17a1002d9392cf
Josh Vote <Josh.Vote@csiro.au>
2011-07-20 01:37:25 +0000

    Merged CSWRecord unit tests from VEGL

--- src/main/java/org/auscope/portal/csw/CSWRecord.java
+++ src/main/java/org/auscope/portal/csw/CSWRecord.java
@@ -226,11 +226,18 @@ public class CSWRecord {
      * @return
      */
     public boolean containsAnyOnlineResource(CSWOnlineResource.OnlineResourceType... types) {
+        
+        if (onlineResources == null) {
+            return false;
+        }
+        
         for (CSWOnlineResource r : onlineResources) {
-            CSWOnlineResource.OnlineResourceType typeToMatch = r.getType();
-            for (CSWOnlineResource.OnlineResourceType type : types) {
-                if (typeToMatch == type) {
-                    return true;
+            if (r != null) {
+                CSWOnlineResource.OnlineResourceType typeToMatch = r.getType();
+                for (CSWOnlineResource.OnlineResourceType type : types) {
+                    if (typeToMatch == type) {
+                        return true;
+                    }
                 }
             }
         }
@@ -245,11 +252,6 @@ public class CSWRecord {
      * @return true if this record contains the given descriptive keyword, false otherwise.
      */
     public boolean containsKeyword(String str) {
-		for(String keyword : descriptiveKeywords) {
-			if(keyword.equals(str)) {
-				return true;
-			}
-		}
-    	return false;
+        return Arrays.asList(descriptiveKeywords).contains(str);
     }
 }
--- src/test/java/org/auscope/portal/csw/TestCSWRecord.java
+++ src/test/java/org/auscope/portal/csw/TestCSWRecord.java
@@ -1,6 +1,8 @@
 package org.auscope.portal.csw;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -14,8 +16,61 @@ import org.junit.Test;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
+/**
+ * Unit tests for CSWRecord
+ * @author Josh Vote
+ *
+ */
 public class TestCSWRecord {
 
+    /**
+     * Tests that containsKeyword returns valid results
+     */
+    @Test
+    public void testContainsKeyword() {
+        CSWRecord record = new CSWRecord("serviceName", "contactOrg", "fileId", "http://record.info", "Abstract", null, null);
+        
+        final String[] descriptiveKeywords = new String[] {"keyword1", "keyworda", "keywordX", null, "", "keyword$"};
+        record.setDescriptiveKeywords(descriptiveKeywords);
+        
+        for (String kw : descriptiveKeywords) {
+            Assert.assertTrue(record.containsKeyword(kw));
+        }
+    }
     
+    @Test
+    public void testContainsAnyOnlineResource() throws MalformedURLException {
+        final CSWOnlineResource[] emptyOnlineResources = new CSWOnlineResource[] {};
+        final CSWOnlineResource[] nullOnlineResources = null;
+        final CSWOnlineResource[] fullOnlineResources = new CSWOnlineResource[] {
+                new CSWOnlineResourceImpl(new URL("http://example.com"), "wfs", "or1", "or1"),
+                new CSWOnlineResourceImpl(new URL("http://example.com"), "wfs", "or2", "or2"),
+                new CSWOnlineResourceImpl(new URL("http://example.com"), "wms", "or3", "or3"),
+                null,
+                new CSWOnlineResourceImpl(new URL("http://example.com"), "unknown", "or4", "or4"),
+        };
+        CSWRecord record = new CSWRecord("serviceName", "contactOrg", "fileId", "http://record.info", "Abstract", null, null);
+        
+        record.setOnlineResources(emptyOnlineResources);
+        Assert.assertFalse(record.containsAnyOnlineResource());
+        Assert.assertFalse(record.containsAnyOnlineResource(CSWOnlineResource.OnlineResourceType.WCS));
+        Assert.assertFalse(record.containsAnyOnlineResource(CSWOnlineResource.OnlineResourceType.WFS));
+        Assert.assertFalse(record.containsAnyOnlineResource(CSWOnlineResource.OnlineResourceType.WMS));
+        Assert.assertFalse(record.containsAnyOnlineResource(CSWOnlineResource.OnlineResourceType.WMS, CSWOnlineResource.OnlineResourceType.WFS, CSWOnlineResource.OnlineResourceType.WCS));
+        
+        record.setOnlineResources(nullOnlineResources);
+        Assert.assertFalse(record.containsAnyOnlineResource());
+        Assert.assertFalse(record.containsAnyOnlineResource(CSWOnlineResource.OnlineResourceType.WCS));
+        Assert.assertFalse(record.containsAnyOnlineResource(CSWOnlineResource.OnlineResourceType.WFS));
+        Assert.assertFalse(record.containsAnyOnlineResource(CSWOnlineResource.OnlineResourceType.WMS));
+        Assert.assertFalse(record.containsAnyOnlineResource(CSWOnlineResource.OnlineResourceType.WMS, CSWOnlineResource.OnlineResourceType.WFS, CSWOnlineResource.OnlineResourceType.WCS));
+        
+        record.setOnlineResources(fullOnlineResources);
+        Assert.assertFalse(record.containsAnyOnlineResource());
+        Assert.assertFalse(record.containsAnyOnlineResource(CSWOnlineResource.OnlineResourceType.WCS));
+        Assert.assertTrue(record.containsAnyOnlineResource(CSWOnlineResource.OnlineResourceType.WFS));
+        Assert.assertTrue(record.containsAnyOnlineResource(CSWOnlineResource.OnlineResourceType.WMS));
+        Assert.assertTrue(record.containsAnyOnlineResource(CSWOnlineResource.OnlineResourceType.WMS, CSWOnlineResource.OnlineResourceType.WFS, CSWOnlineResource.OnlineResourceType.WCS));
+    }
 
 }
