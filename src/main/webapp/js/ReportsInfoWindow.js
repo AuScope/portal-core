4c5cac787658a37ba4f6b3adbe19b26851e1fa22
Josh Vote <Josh.Vote@csiro.au>
2011-08-19 06:39:31 +0000

    Upgrade to reports to utilise the new CSWMetadataPanel widget

--- src/main/webapp/js/ReportsInfoWindow.js
+++ src/main/webapp/js/ReportsInfoWindow.js
@@ -18,41 +18,53 @@ ReportsInfoWindow.prototype = {
 //		'white-space:-pre-wrap;white-space:-o-pre-wrap;word-wrap:break-word;' +
 //		'width:99%;max-height:300px;overflow:auto;">' +
 
-	'show': function() {
-		var recordLink = this.cswRecord.getRecordInfoUrl();
-		var recordLinkName = "Link back to registry";
-		var sHtml = '<div class="niceDivOverFlow">' +
-			'<table border="1" cellspacing="1" cellpadding="4" class="auscopeTable">' +
-			"<tr><td>" +
-		     "<a href='" + recordLink + "' target='_blank'>" +
-		     recordLinkName +
-		     "</a>" +
-		     "</td></tr>"+
-			'<tr><td id="reportsname">' +
-			this.cswRecord.getServiceName() +
-			'</td></tr>' +
-			'<tr><td id="reportsabstract">' +
-			this.cswRecord.getDataIdentificationAbstract() +
-			'</td></tr>';
+    'show': function() {
+        var divId = 'reports-binding-id';
+        var sHtml = '';
+        var maxWidth = 600;
+        var maxHeight = 360;
 
-		var wwwLinks = this.cswRecord.getFilteredOnlineResources('WWW');
-		for(var i=0; i< wwwLinks.length; i++) {
-			var linkName = wwwLinks[i].name !== null && wwwLinks[i].name !== "" ? wwwLinks[i].name : wwwLinks[i].url;
-			sHtml += "<tr><td>" +
-				     "<a href='"	+ wwwLinks[i].url + "' target='_blank'>" +
-				     linkName +
-				     "</a>" +
-				     "</td></tr>";
-		}
-		
+        sHtml += '<div id="' + divId + '" style="width: ' + maxWidth + 'px; height: ' + maxHeight  +'px;"/>';
 
-		sHtml += "</table></div>";
-		if (this.overlay instanceof GPolygon) {
-			this.map.openInfoWindowHtml(this.overlay.getBounds().getCenter(), sHtml,
-					{maxWidth:800, maxHeight:400, autoScroll:true});
-		} else if (this.overlay instanceof GMarker) {
-			this.map.openInfoWindowHtml(this.overlay.getPoint(), sHtml,
-					{maxWidth:800, maxHeight:400, autoScroll:true});
-		}
-	}
+        var renderMetadataPanel = function() {
+            this.reportsPanel = new Ext.form.FormPanel({
+                width : maxWidth - 20,
+                height : maxHeight - 20,
+                renderTo : divId,
+                hideBorders : true,
+                layout : 'fit',
+                items : [{
+                    xtype : 'cswmetadatapanel',
+                    cswRecord : this.cswRecord,
+                    bodyStyle : {
+                        'background-color' : '#ffffff'
+                    }
+                }]
+            });
+        }
+
+        var destroyReportsPanel = function() {
+            this.reportsPanel.destroy();
+        }
+
+        if (this.overlay instanceof GPolygon) {
+            this.map.openInfoWindowHtml(this.overlay.getBounds().getCenter(), sHtml, {
+                maxWidth:maxWidth,
+                maxHeight:maxHeight,
+                autoScroll:true,
+                onOpenFn : renderMetadataPanel.createDelegate(this),
+                onCloseFn : destroyReportsPanel.createDelegate(this)
+            });
+        } else if (this.overlay instanceof GMarker) {
+            this.map.openInfoWindowHtml(this.overlay.getPoint(), sHtml,{
+                maxWidth:maxWidth,
+                maxHeight:maxHeight,
+                autoScroll:true,
+                onOpenFn : renderMetadataPanel.createDelegate(this),
+                onCloseFn : destroyReportsPanel.createDelegate(this)
+            });
+        }
+
+
+    }
 };
