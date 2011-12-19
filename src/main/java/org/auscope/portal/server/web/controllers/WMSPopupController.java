b0fb4fc79b43e3119d38db88e090ba3f7be28db9
Josh Vote <Josh.Vote@csiro.au>
2011-12-19 02:07:49 +0000

    WMS popup controller now uses WMS Method Maker

--- src/main/java/org/auscope/portal/server/web/controllers/WMSPopupController.java
+++ src/main/java/org/auscope/portal/server/web/controllers/WMSPopupController.java
@@ -1,14 +1,20 @@
 package org.auscope.portal.server.web.controllers;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.httpclient.HttpMethodBase;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.auscope.portal.server.util.FileIOUtil;
+import org.auscope.portal.server.web.WMSMethodMaker;
 import org.auscope.portal.server.web.service.HttpServiceCaller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,9 +28,36 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 
 @Controller
-public class WMSPopupController {
+public class WMSPopupController extends BasePortalController {
    private final Log log = LogFactory.getLog(getClass());
 
+   private HttpServiceCaller serviceCaller;
+
+   /**
+    * Creates a new controller
+    * @param serviceCaller will be used for making external requests
+    */
+   @Autowired
+   public WMSPopupController(HttpServiceCaller serviceCaller) {
+       this.serviceCaller = serviceCaller;
+   }
+
+   /**
+    *
+    * @param request
+    * @param response
+    * @param wmsUrl
+    * @param latitude
+    * @param longitude
+    * @param queryLayers
+    * @param x
+    * @param y
+    * @param bbox A CSV string formatted in the form - longitude,latitude,longitude,latitude
+    * @param width
+    * @param height
+    * @param infoFormat
+    * @throws Exception
+    */
    @RequestMapping("/wmsMarkerPopup.do")
    public void wmsUnitPopup(HttpServletRequest request,
                             HttpServletResponse response,
@@ -37,38 +70,41 @@ public class WMSPopupController {
                             @RequestParam("BBOX") String bbox,
                             @RequestParam("WIDTH") String width,
                             @RequestParam("HEIGHT") String height,
-                            @RequestParam("INFO_FORMAT") String infoFormat) throws IOException
-   {
-      String AMP = "&";
-      String url = wmsUrl;
-      // "&" character cannot be passed within url string
-      // We need this check for tricky urls such as Geoscience Australia.
-      if (!url.endsWith("?") || !url.endsWith("&") || !url.endsWith("=")) {
-         if (url.contains("?"))
-            url += AMP;
-         else
-            url += "?";
+                            @RequestParam("INFO_FORMAT") String infoFormat) throws Exception {
+
+      String[] bboxParts = bbox.split(",");
+      double lng1 = Double.parseDouble(bboxParts[0]);
+      double lng2 = Double.parseDouble(bboxParts[2]);
+      double lat1 = Double.parseDouble(bboxParts[1]);
+      double lat2 = Double.parseDouble(bboxParts[3]);
+
+
+      WMSMethodMaker methodMaker = new WMSMethodMaker(wmsUrl);
+      HttpMethodBase method = methodMaker.getFeatureInfo(infoFormat,
+              queryLayers,
+              "EPSG:4326",
+              Math.min(lng1, lng2), //west
+              Math.min(lat1, lat2), //south
+              Math.max(lng1, lng2), //east
+              Math.max(lat1, lat2), //north
+              Integer.parseInt(width),
+              Integer.parseInt(height),
+              Double.parseDouble(longitude),
+              Double.parseDouble(latitude),
+              Integer.parseInt(x),
+              Integer.parseInt(y),
+              "");
+
+
+      InputStream responseStream = null;
+
+      try {
+          responseStream = serviceCaller.getMethodResponseAsStream(method, serviceCaller.getHttpClient());
+          this.writeInputToOutputStream(responseStream, response.getOutputStream(), 1024*1024);
+      } finally {
+          if (responseStream != null) {
+              responseStream.close();
+          }
       }
-      url += "REQUEST=GetFeatureInfo&EXCEPTIONS=application/vnd.ogc.se_xml";
-      url += "&VERSION=1.1.0";
-      url += "&BBOX=" + bbox;
-      url += "&X=" + x + "&Y=" + y;
-      url += "&INFO_FORMAT=" + infoFormat;
-      url += "&QUERY_LAYERS=" + queryLayers;
-      url += "&FEATURE_COUNT=50";
-      url += "&SRS=EPSG:4326";
-      url += "&LAYERS=" + queryLayers;
-      url += "&STYLES=";      // Ask server for default style
-      url += "&WIDTH=" + width + "&HEIGHT=" + height;
-      url += "&FORMAT=image/png";
-      log.debug(url);
-
-      HttpServiceCaller serviceCaller = new HttpServiceCaller();
-      String responseFromCall = serviceCaller.callHttpUrlGET(new URL(url));
-
-      log.debug(responseFromCall);
-
-      // Send response back to client
-      response.getWriter().write(responseFromCall);
    }
 }
