1eb4ccde5c719d75f21f01f7c38463cfaf88bcd6
Adam Brown <Adam.Brown@csiro.au>
2013-05-06 06:40:27 +0000

    AGOS-35: Removed defaultAnyTextFilter. That was a mistake - the default filter is meant to be applied earlier.

--- src/main/java/org/auscope/portal/core/services/csw/CSWServiceItem.java
+++ src/main/java/org/auscope/portal/core/services/csw/CSWServiceItem.java
@@ -20,7 +20,6 @@ public class CSWServiceItem {
     private String userName;
     private String password;
     private String cqlText;
-    private String defaultAnyTextFilter;
     private String[] defaultConstraints;
     private boolean noCache = false;
 
@@ -237,22 +236,6 @@ public class CSWServiceItem {
      */
     public void setCqlText(String cqlText) {
         this.cqlText = cqlText;
-    }
-    
-    /**
-     * Get the defaultAnyTextFilter for querying the catalog.
-     * @return String
-     */
-    public String getDefaultAnyTextFilter() {
-        return defaultAnyTextFilter;
-    }
-    
-    /**
-     * Set defaultAnyTextFilter for querying the catalog.
-     * @return
-     */
-    public void setDefaultAnyTextFilter(String defaultAnyTextFilter) {
-        this.defaultAnyTextFilter = defaultAnyTextFilter;
     }
     
     /**
