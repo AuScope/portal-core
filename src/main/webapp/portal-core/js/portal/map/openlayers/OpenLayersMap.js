1a7370f2517022f84a1a27d1a2365a98eab99a1b
Richard Goh <Richard.Goh@csiro.au>
2013-07-09 01:12:40 +0000

    Added auto expanding of bounding box fieldset. (AUS-2321)

--- src/main/webapp/portal-core/js/portal/map/openlayers/OpenLayersMap.js
+++ src/main/webapp/portal-core/js/portal/map/openlayers/OpenLayersMap.js
@@ -356,14 +356,19 @@ Ext.define('portal.map.openlayers.OpenLayersMap', {
         customNavTb.controls[1].events.on({
             "afterZoom": function() {
                // populate bounding box fields if presence with lon/lat bounds of current selected view area.
-               //cswServiceFilterForm = Ext.ComponentQuery.query('form#cswServiceFilterForm')[0];
                var cswServiceFilterForm = Ext.getCmp('cswServiceFilterForm');
                if (cswServiceFilterForm) {
+                   // expand bounding box fieldset if it is not already so
+                   var bboxFieldset = Ext.getCmp('bboxFieldset');
+                   if (bboxFieldset.collapsed) {
+                       bboxFieldset.expand();
+                   }
+                   // get the extent and populate the bounding box fields
                    bounds = this.map.getExtent().transform('EPSG:3857','EPSG:4326').toArray();
                    Ext.getCmp("lat_max").setValue(bounds[3]); // north bound latitude
                    Ext.getCmp("lat_min").setValue(bounds[1]); // south bound latitude
                    Ext.getCmp("long_max").setValue(bounds[2]); // east bound longitude
-                   Ext.getCmp("long_min").setValue(bounds[0]); // west bound longitude               
+                   Ext.getCmp("long_min").setValue(bounds[0]); // west bound longitude
                }
                
                // reset the control to panning.
