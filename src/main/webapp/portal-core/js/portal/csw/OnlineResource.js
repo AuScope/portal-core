186da488851d3d8d3413f4575767d4c2a96303c3
Victor Tey <Victor.Tey@csiro.au>
2013-10-31 07:50:23 +0000

    Revert changes to wms 1.3

--- src/main/webapp/portal-core/js/portal/csw/OnlineResource.js
+++ src/main/webapp/portal-core/js/portal/csw/OnlineResource.js
@@ -20,8 +20,7 @@ Ext.define('portal.csw.OnlineResource', {
 
     //Static value representations of the 'type' field
     statics : {
-        WMS : 'WMS', //represents a Web Map Service 1.1.1
-        WMS3 : 'WMS3', //represents a Web Map Service 1.3.0
+        WMS : 'WMS', //represents a Web Map Service
         WFS : 'WFS', //represents a Web Feature Service
         WCS : 'WCS', //represents a Web Coverage Service
         WWW : 'WWW', //represents a regular HTTP web link
@@ -45,8 +44,6 @@ Ext.define('portal.csw.OnlineResource', {
                 return 'OGC Web Feature Service 1.1.0';
             case portal.csw.OnlineResource.WMS:
                 return 'OGC Web Map Service 1.1.1';
-            case portal.csw.OnlineResource.WMS3:
-                return 'OGC Web Map Service 1.3.0';
             case portal.csw.OnlineResource.WCS:
                 return 'OGC Web Coverage Service 1.0.0';
             case portal.csw.OnlineResource.OPeNDAP:
