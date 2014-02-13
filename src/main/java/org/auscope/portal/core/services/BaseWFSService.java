2e25b4c83cac155eb2a011cd8583f0e18bc9c57c
Victor Tey <Victor.Tey@csiro.au>
2014-02-13 09:17:51 +0000

    AUS-2441:Add minocc view and tenements into the portal layer

--- src/main/java/org/auscope/portal/core/services/BaseWFSService.java
+++ src/main/java/org/auscope/portal/core/services/BaseWFSService.java
@@ -147,21 +147,21 @@ public abstract class BaseWFSService {
             throw new PortalServiceException(method, ex);
         }
     }
-    
+
     public WFSGetCapabilitiesResponse getCapabilitiesResponse(String wfsUrl) throws PortalServiceException {
         HttpRequestBase method = null;
-        
+
         try {
             //Make WFS request
             method = wfsMethodMaker.makeGetCapabilitiesMethod(wfsUrl);
             String responseString = httpServiceCaller.getMethodResponseAsString(method);
-            
+
             //Parse resulting XML
             Document responseDoc = DOMUtil.buildDomFromString(responseString);
             OWSExceptionParser.checkForExceptionResponse(responseDoc);
-            
+
             WFSGetCapabilitiesResponse parsedGetCap = new WFSGetCapabilitiesResponse();
-            
+
             //Get the output formats
             XPathExpression xPathGetOf = DOMUtil.compileXPathExpr("wfs:WFS_Capabilities/ows:OperationsMetadata/ows:Operation[@name=\"GetFeature\"]/ows:Parameter[@name=\"outputFormat\"]/ows:Value", new WFSNamespaceContext());
             NodeList formatNodes = (NodeList) xPathGetOf.evaluate(responseDoc, XPathConstants.NODESET);
@@ -170,7 +170,7 @@ public abstract class BaseWFSService {
                 outputFormats[i] = formatNodes.item(i).getTextContent();
             }
             parsedGetCap.setGetFeatureOutputFormats(outputFormats);
-            
+
             //Get feature type names
             XPathExpression xPathGetTn = DOMUtil.compileXPathExpr("wfs:WFS_Capabilities/wfs:FeatureTypeList/wfs:FeatureType/wfs:Name", new WFSNamespaceContext());
             NodeList nameNodes = (NodeList) xPathGetTn.evaluate(responseDoc, XPathConstants.NODESET);
@@ -179,7 +179,7 @@ public abstract class BaseWFSService {
                 typeNames[i] = nameNodes.item(i).getTextContent();
             }
             parsedGetCap.setFeatureTypes(typeNames);
-            
+
             return parsedGetCap;
         } catch (Exception e) {
             throw new PortalServiceException(method, e);
@@ -188,6 +188,28 @@ public abstract class BaseWFSService {
                 method.releaseConnection();
             }
         }
-        
+
+    }
+
+    /**
+     * Download a wfs based on the type and filter.
+     * @param serviceUrl a Web Feature Service URL
+     * @param type
+     * @param filterString
+     * @param maxFeatures The maximum number of features to request
+     * @return
+     * @throws PortalServiceException
+     */
+    public InputStream downloadWFS(String serviceUrl,String type, String filterString, Integer maxFeatures) throws PortalServiceException {
+
+        HttpRequestBase method = null;
+        try {
+            //VT: I did not include the namespace for mineralTenement but the query worked as tested.
+            method = generateWFSRequest(serviceUrl, type, null, filterString, maxFeatures, null, ResultType.Results);
+            return httpServiceCaller.getMethodResponseAsStream(method);
+
+        } catch (Exception ex) {
+            throw new PortalServiceException(method, "Error when attempting to download from:" + serviceUrl , ex);
+        }
     }
 }
