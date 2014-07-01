95181e38bfc7a2f38ccda20cdb5c7d9199e79b0f
Victor Tey <Victor.Tey@csiro.au>
2014-07-01 04:46:30 +0000

    Add Aster chile

--- src/main/webapp/portal-core/js/portal/layer/querier/wms/WMSQuerier.js
+++ src/main/webapp/portal-core/js/portal/layer/querier/wms/WMSQuerier.js
@@ -55,6 +55,100 @@ Ext.define('portal.layer.querier.wms.WMSQuerier', {
         return lowerCase.length > 32;
     },
 
+
+    /**
+     * This is for creating a Node Objects from a DOM Node in the form
+     * {
+     *  text : String
+     *  leaf : Boolean
+     * }
+     */
+    _createTreeNode : function(documentNode) {
+        var treeNode = null;
+
+        // We have a leaf
+        if (portal.util.xml.SimpleDOM.isLeafNode(documentNode)) {
+            var textContent = portal.util.xml.SimpleDOM.getNodeTextContent(documentNode);
+
+            treeNode = {
+                text : documentNode.tagName + " = " + textContent,
+                children : [],
+                leaf: true
+            };
+        } else { // we have a parent node
+            var parentName = documentNode.tagName;
+            if (documentNode.attributes.length > 0) {
+                parentName += '(';
+                for ( var i = 0; i < documentNode.attributes.length; i++) {
+                    parentName += ' ' + documentNode.attributes[i].nodeName +
+                                  '=' + documentNode.attributes[i].value;
+                }
+                parentName += ')';
+            }
+            treeNode = {
+                text : parentName,
+                children : [],
+                leaf: true
+            };
+        }
+
+        return treeNode;
+    },
+
+    /**
+     * Given a DOM tree starting at xmlDocNode, this function returns the
+     * equivelant tree in ExtJs Tree Nodes
+     */
+    _parseXmlTree : function(xmlDocNode, treeNode) {
+        var nodes = [];
+        Ext.each(xmlDocNode.childNodes, function(docNodeChild) {
+            if (docNodeChild.nodeType == portal.util.xml.SimpleDOM.XML_NODE_ELEMENT) {
+                var treeChildNode = this._createTreeNode(docNodeChild);
+                treeNode.leaf = false;
+                treeNode.children.push(treeChildNode);
+                nodes.push(treeNode);
+                this._parseXmlTree(docNodeChild, treeChildNode);
+            }
+        }, this);
+
+        return nodes;
+    },
+
+    _parseStringXMLtoTreePanel : function(xmlString){
+        var domNode = portal.util.xml.SimpleDOM.parseStringToDOM(xmlString);
+        var rootNode = this._createTreeNode(domNode.documentElement);
+        this._parseXmlTree(domNode.documentElement, rootNode);
+        rootNode.expanded = true;
+
+        // Continuously expand child nodes until we hit a node with
+        // something "interesting" defined as a node with more than 1 child
+        if (rootNode.children.length == 1) {
+            var childNode = rootNode.children[0];
+            while (childNode) {
+                childNode.expanded = true;
+
+                if (childNode.children.length > 1) {
+                    break;
+                } else {
+                    childNode = childNode.children[0];
+                }
+            }
+        }
+
+        var panelConfig = {
+            layout : 'fit',
+            height: 300,
+            items : [{
+                xtype : 'treepanel',
+                autoScroll : true,
+                rootVisible : true,
+                root : rootNode
+            }]
+        };
+
+        return panelConfig;
+    },
+
     /**
      * See parent class for definition
      *
