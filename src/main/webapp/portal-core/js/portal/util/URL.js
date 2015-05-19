d27dd340193871d14bd0d144acf7ac174d62b234
joshvote <Josh.Vote@csiro.au>
2015-05-19 14:09:06 +0800

    Improved support for ROOT context

--- src/main/webapp/portal-core/js/portal/util/URL.js
+++ src/main/webapp/portal-core/js/portal/util/URL.js
@@ -4,7 +4,7 @@
 Ext.define('portal.util.URL', {
     singleton: true
 }, function() {
-    if (!window.WEB_CONTEXT) {
+    if (!Ext.isString(window.WEB_CONTEXT)) {
         window.WEB_CONTEXT = '/' + window.location.pathname.substring(1).split("/")[0];
     }
 
