3f412c5d9598873d478b0f860ea0e17672d33e0f
Heya <heya.gosper@ga.gov.au>
2015-10-20 07:14:24 +1100

    GPT-49 Added a reset form button to the search window

--- src/main/webapp/portal-core/js/portal/widgets/window/CSWFilterWindow.js
+++ src/main/webapp/portal-core/js/portal/widgets/window/CSWFilterWindow.js
@@ -4,51 +4,68 @@ Ext.define('portal.widgets.window.CSWFilterWindow', {
 
     cswFilterFormPanel : null,
     
+    controlButtons: [],
+    
     constructor : function(cfg) {      
 
+    	var me = this;
+    	
         // use the search panel defined in the config if present, otherwise use the Auscope Core default
-        this.cswFilterFormPanel = cfg.cswFilterFormPanel || new portal.widgets.panel.CSWFilterFormPanel({
+    	me.cswFilterFormPanel = cfg.cswFilterFormPanel || new portal.widgets.panel.CSWFilterFormPanel({
             name : 'Filter Form'
         });
+        
+        // Only add a reset button if it is GA Portal 
+        // Auscope may want this too but would need to implement the resetForm function in CSWFilterFormPanel.js
+        if (me.cswFilterFormPanel.resetForm) {
+        	me.controlButtons.push({
+                xtype: 'button',
+                text:'Reset Form',
+                handler:function(button){
+                    me.cswFilterFormPanel.resetForm();                        
+                }
+        	});
+        };
+        	
+        me.controlButtons.push({            
+            xtype: 'button',
+            text: 'Search',
+            scope : me,
+            iconCls : 'add',
+            handler: function(button) {
+                var parent = button.findParentByType('window');
+                var panel = parent.getComponent(0);
 
+                if (panel.getForm().isValid()) {                 
+                    var additionalParams = panel.getForm().getValues(false, false, false, false);
+                    var filteredResultPanels=[];
+                    for(additionalParamKey in additionalParams){
+                        if(additionalParamKey == 'cswServiceId'){
+                            if(!(additionalParams[additionalParamKey] instanceof Array)){
+                                additionalParams[additionalParamKey]=[additionalParams[additionalParamKey]]
+                            }
+                            for(var j=0; j < additionalParams[additionalParamKey].length;j++){
+                                //VT:
+                                filteredResultPanels.push(this._getTabPanels(additionalParams,additionalParams[additionalParamKey][j]));
+                            }
+                        }
+                    }
+                    parent.fireEvent('filterselectcomplete',filteredResultPanels);
+                    parent.hide();  
+                    
+                } else {
+                    Ext.Msg.alert('Invalid Data', 'Please correct form errors.')
+                }
+            }
+        });	
+        
         Ext.apply(cfg, {
             title : 'Enter Parameters',
             layout : 'fit',
             modal : false,
             width : 500,
-            items : [this.cswFilterFormPanel],
-            buttons:[{
-                xtype: 'button',
-                text: 'Search',
-                scope : this,
-                iconCls : 'add',
-                handler: function(button) {
-                    var parent = button.findParentByType('window');
-                    var panel = parent.getComponent(0);
-
-                    if (panel.getForm().isValid()) {                 
-                        var additionalParams = panel.getForm().getValues(false, false, false, false);
-                        var filteredResultPanels=[];
-                        for(additionalParamKey in additionalParams){
-                            if(additionalParamKey == 'cswServiceId'){
-                                if(!(additionalParams[additionalParamKey] instanceof Array)){
-                                    additionalParams[additionalParamKey]=[additionalParams[additionalParamKey]]
-                                }
-                                for(var j=0; j < additionalParams[additionalParamKey].length;j++){
-                                    //VT:
-                                    filteredResultPanels.push(this._getTabPanels(additionalParams,additionalParams[additionalParamKey][j]));
-                                }
-                            }
-                        }
-                        parent.fireEvent('filterselectcomplete',filteredResultPanels);
-                        parent.hide();  
-                        
-                    } else {
-                        Ext.Msg.alert('Invalid Data', 'Please correct form errors.')
-                    }
-                }
-            }]
-            
+            items : [me.cswFilterFormPanel],
+            buttons: me.controlButtons            
         });
 
 
