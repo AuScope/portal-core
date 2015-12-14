95318f5044f92427f51114719d9c01d67a45dcd9
Carsten Friedrich <Carsten.Friedrich@csiro.au>
2015-12-14 16:34:13 +1100

    config.properties now supports definition of properties with prefix 'DEFAULT'. If not host specific entry is found, the 'DEFAULT' value is used (if available)

--- src/main/java/org/auscope/portal/core/server/PortalPropertyPlaceholderConfigurer.java
+++ src/main/java/org/auscope/portal/core/server/PortalPropertyPlaceholderConfigurer.java
@@ -80,7 +80,15 @@ public class PortalPropertyPlaceholderConfigurer extends PropertyPlaceholderConf
                 log.debug("Translated property: " + property);
 
                 String prop = props.getProperty(property);
+                
+                if(prop==null) { // Can't find hostname entry. Let's try for default
+                	log.info("Can't find hostname specific property, trying to find default for: "+property);
+                    property = placeholder.replaceFirst("HOST", "DEFAULT");
 
+                    log.debug("Default property: " + property);
+
+                    prop = props.getProperty(property);                	
+                }
                 if (prop == null)
                     log.warn("Please define property: " + property);
 
