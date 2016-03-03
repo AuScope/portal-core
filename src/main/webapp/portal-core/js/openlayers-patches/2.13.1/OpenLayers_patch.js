d2821654e58a1a60701649e385fc821841818a43
joshvote <Josh.Vote@csiro.au>
2016-03-03 15:12:16 +0800

    Fixed JS error when openlayers is not present

--- src/main/webapp/portal-core/js/openlayers-patches/2.13.1/OpenLayers_patch.js
+++ src/main/webapp/portal-core/js/openlayers-patches/2.13.1/OpenLayers_patch.js
@@ -12,51 +12,52 @@
  * 
  * This file applies the patch by overriding the containing function in the OpenLayers object.
  */
-
-/**
- * Method: setGMapVisibility
- * Display the GMap container and associated elements.
- * 
- * Parameters:
- * visible - {Boolean} Display the GMap elements.
- */
-OpenLayers.Layer.Google.v3.setGMapVisibility = function(visible) {
-    var cache = OpenLayers.Layer.Google.cache[this.map.id];
-    var map = this.map;
-    if (cache) {
-        var type = this.type;
-        var layers = map.layers;
-        var layer;
-        for (var i=layers.length-1; i>=0; --i) {
-            layer = layers[i];
-            if (layer instanceof OpenLayers.Layer.Google &&
-                        layer.visibility === true && layer.inRange === true) {
-                type = layer.type;
-                visible = true;
-                break;
-            }
-        }
-        var container = this.mapObject.getDiv();
-        if (visible === true) {
-            if (container.parentNode !== map.div) {
-                if (!cache.rendered) {
-                    var me = this;
-                    google.maps.event.addListenerOnce(this.mapObject, 'tilesloaded', function() {
-                        cache.rendered = true;
-                        me.setGMapVisibility(me.getVisibility());
-                        me.moveTo(me.map.getCenter());
-                        cache.googleControl.appendChild(map.viewPortDiv);
-                    });
-                } else {
-                    cache.googleControl.appendChild(map.viewPortDiv);
+if (window.OpenLayers) {
+    /**
+     * Method: setGMapVisibility
+     * Display the GMap container and associated elements.
+     * 
+     * Parameters:
+     * visible - {Boolean} Display the GMap elements.
+     */
+    OpenLayers.Layer.Google.v3.setGMapVisibility = function(visible) {
+        var cache = OpenLayers.Layer.Google.cache[this.map.id];
+        var map = this.map;
+        if (cache) {
+            var type = this.type;
+            var layers = map.layers;
+            var layer;
+            for (var i=layers.length-1; i>=0; --i) {
+                layer = layers[i];
+                if (layer instanceof OpenLayers.Layer.Google &&
+                            layer.visibility === true && layer.inRange === true) {
+                    type = layer.type;
+                    visible = true;
+                    break;
                 }
-                map.div.appendChild(container);
-                google.maps.event.trigger(this.mapObject, 'resize');
             }
-            this.mapObject.setMapTypeId(type);                
-        } else if (cache.googleControl.hasChildNodes()) {
-            map.div.appendChild(map.viewPortDiv);
-            map.div.removeChild(container);
+            var container = this.mapObject.getDiv();
+            if (visible === true) {
+                if (container.parentNode !== map.div) {
+                    if (!cache.rendered) {
+                        var me = this;
+                        google.maps.event.addListenerOnce(this.mapObject, 'tilesloaded', function() {
+                            cache.rendered = true;
+                            me.setGMapVisibility(me.getVisibility());
+                            me.moveTo(me.map.getCenter());
+                            cache.googleControl.appendChild(map.viewPortDiv);
+                        });
+                    } else {
+                        cache.googleControl.appendChild(map.viewPortDiv);
+                    }
+                    map.div.appendChild(container);
+                    google.maps.event.trigger(this.mapObject, 'resize');
+                }
+                this.mapObject.setMapTypeId(type);                
+            } else if (cache.googleControl.hasChildNodes()) {
+                map.div.appendChild(map.viewPortDiv);
+                map.div.removeChild(container);
+            }
         }
-    }
-};
+    };
+}
