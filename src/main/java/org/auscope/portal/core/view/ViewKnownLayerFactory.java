c5f77095cba2a1fec3b993974735aacacaf4fa18
heyagosper <heya.gosper@ga.gov.au>
2016-03-22 13:13:09 +1100

    GPT-174 support single tile

--- src/main/java/org/auscope/portal/core/view/ViewKnownLayerFactory.java
+++ src/main/java/org/auscope/portal/core/view/ViewKnownLayerFactory.java
@@ -48,7 +48,9 @@ public class ViewKnownLayerFactory {
         obj.put("feature_count", k.getFeature_count());
 
         obj.put("order", k.getOrder());
-
+        
+        obj.put("singleTile", k.getSingleTile());
+        
         String group = "Others";
         if (k.getGroup() != null && !k.getGroup().isEmpty()) {
             group = k.getGroup();
