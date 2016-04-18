7f6ed8c9618db4c3b412ac103ad82827be591733
comtesacristain <mick.sexton@gmail.com>
2016-04-18 15:40:50 +1000

    GPT-104 Refactor
    Remove code not used in this implementation.

--- src/main/java/org/auscope/portal/core/view/knownlayer/WMSWFSSelector.java
+++ src/main/java/org/auscope/portal/core/view/knownlayer/WMSWFSSelector.java
@@ -8,23 +8,9 @@ public class WMSWFSSelector implements KnownLayerSelector {
 	private String featureTypeName;
 	private String layerName;
 
-	private String[] relatedRecords;
-	
-	private String[] serviceEndpoints;
-	private boolean includeEndpoints;
-
 	public WMSWFSSelector(String featureTypeName, String layerName) {
 		this.featureTypeName = featureTypeName;
 		this.layerName = layerName;
-	}
-	
-	public WMSWFSSelector(String featureTypeName, String layerName, String[] serviceEndpoints,
-			boolean includeEndpoints) {
-
-		this.featureTypeName = featureTypeName;
-		this.layerName = layerName;
-		this.serviceEndpoints = serviceEndpoints;
-		this.includeEndpoints = includeEndpoints;
 	}
 
 	@Override
@@ -34,50 +20,14 @@ public class WMSWFSSelector implements KnownLayerSelector {
 		// Check for strong association to begin with
 		for (AbstractCSWOnlineResource onlineResource : wmsResources) {
 			if (layerName.equals(onlineResource.getName()) || featureTypeName.equals(onlineResource.getName())) {
-				// OK we have a match, check we don't explicitly/implicitly
-				// exclude it
-				// based on its URL
-				if (serviceEndpoints != null && serviceEndpoints.length > 0) {
-					boolean matched = false;
-					for (String url : serviceEndpoints) {
-						if (url.equals(onlineResource.getLinkage().toString())) {
-							matched = true;
-							break;
-						}
-					}
 
-					// Our list of endpoints will be saying either
-					// 'Include only this list of urls'
-					// 'Exclude any of these urls'
-					if ((includeEndpoints && matched) || (!includeEndpoints && !matched)) {
-						return RelationType.Belongs;
-					}
-				} else {
-					// Otherwise this knownlayer makes no restrictions on URL
-					return RelationType.Belongs;
-				}
-			}
-		}
+				return RelationType.Belongs;
 
-		
-		// next we check for a weaker relation (does the type name exist in
-		// the list of related feature type names?)
-		if (relatedRecords != null) {
-			for (String relatedRecord : relatedRecords) {
-				for (AbstractCSWOnlineResource onlineResource : wmsResources) {
-					if (onlineResource.getName().equals(relatedRecord)) {
-						return RelationType.Related;
-					}
-				}
 			}
 		}
 
 		return RelationType.NotRelated;
 	}
-
-	
-
-	
 
 	public String getFeatureTypeName() {
 		return featureTypeName;
