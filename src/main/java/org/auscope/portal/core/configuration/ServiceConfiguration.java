9e2825c9818171ef70dd9b9b033dac19948a404c
ang05a <rini.angreani@csiro.au>
2017-02-10 16:03:05 +0800

    AUS-2712: Reuse ServiceConfiguration bean to get ERML 2.0 namespace

--- src/main/java/org/auscope/portal/core/configuration/ServiceConfiguration.java
+++ src/main/java/org/auscope/portal/core/configuration/ServiceConfiguration.java
@@ -1,21 +1,11 @@
 package org.auscope.portal.core.configuration;
 
-import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
 public class ServiceConfiguration {
 
     List<ServiceConfigurationItem> serviceConfigurationItems;
     
-    @Autowired
     public ServiceConfiguration(List<ServiceConfigurationItem> serviceConfigurationItems) {
         this.serviceConfigurationItems = serviceConfigurationItems;
     }
@@ -35,22 +25,5 @@ public class ServiceConfiguration {
         }
 
         return null;
-    }     
-    
-    /**
-     * Checks if a service is GML 3.2 based or not. The default is GML 3.1.1 unless configured otherwise. 
-     * @param response
-     * @param url - ServiceConfigurationItem url
-     * @throws IOException
-     */
-    @RequestMapping("/checkGml32.do")    
-    public void isGml32(HttpServletResponse response,
-            @RequestParam("serviceUrl") final String url) throws IOException {
-        boolean isGml32 = false;
-        ServiceConfigurationItem service = getServiceConfigurationItem(url);
-        if (service != null) {
-            isGml32 = service.isGml32();
-        }
-        response.getWriter().write(String.valueOf(isGml32));
     }
 }
