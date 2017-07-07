d9ae1678eb24d1549ab96c2e256e035c9d208a5a
michaelsexton <michael.sexton@ga.gov.au>
2017-07-07 10:37:18 +1000

    Add extra namespaces for vocab RDF parsing

--- src/main/java/org/auscope/portal/core/services/namespaces/VocabNamespaceContext.java
+++ src/main/java/org/auscope/portal/core/services/namespaces/VocabNamespaceContext.java
@@ -11,9 +11,12 @@ public class VocabNamespaceContext extends IterableNamespace {
     public static final String SKOS_NAMESPACE = "http://www.w3.org/2004/02/skos/core#";
     public static final String RDF_NAMESPACE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
     public static final String OWL_NAMESPACE = "http://www.w3.org/2002/07/owl#";
+    public static final String DC_NAMESPACE = "http://purl.org/dc/elements/1.1/";
+    public static final String DCTERMS_NAMESPACE = "http://purl.org/dc/terms/";
+    public static final String GTS_NAMESPACE = "http://resource.geosciml.org/ontology/timescale/gts#";
 
     public VocabNamespaceContext() {
-        map.put("dc", "http://purl.org/dc/elements/1.1/");
+        map.put("dc", DC_NAMESPACE);
         map.put("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
         map.put("xsd", "http://www.w3.org/2001/XMLSchema#");
         map.put("owl", OWL_NAMESPACE);
@@ -23,6 +26,7 @@ public class VocabNamespaceContext extends IterableNamespace {
         map.put("api", "http://purl.org/linked-data/api/vocab#");
         map.put("xhv", "http://www.w3.org/1999/xhtml/vocab#");
         map.put("os", "http://a9.com/-/spec/opensearch/1.1/");
-        map.put("dcterms", "http://purl.org/dc/terms/");
+        map.put("dcterms", DCTERMS_NAMESPACE);
+        map.put("gts", GTS_NAMESPACE);
     }
 }
