ae9bf778ea975e3260903ba3597f15b847b0ecc5
michaelsexton <michael.sexton@ga.gov.au>
2018-06-05 13:14:41 +1000

    Change TYPE in mandatory UI filters

--- src/main/java/org/auscope/portal/core/uifilter/mandatory/UITextBox.java
+++ src/main/java/org/auscope/portal/core/uifilter/mandatory/UITextBox.java
@@ -2,7 +2,7 @@ package org.auscope.portal.core.uifilter.mandatory;
 
 public class UITextBox extends AbstractMandatoryParamBinding {
 
-    public final String TYPE ="MANDATORY.TEXTBOX";
+    private static final String TYPE ="MANDATORY.TEXTBOX";
 
     public UITextBox(String label,String parameter, String value){
         this.setLabel(label);
