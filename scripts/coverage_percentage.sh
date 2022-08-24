8f75b7edf0ae41f4c053088179eeb1c793a5f394
chrisvpeters <chris.peters@csiro.au>
2022-08-24 17:48:26 +1000

    Update coverage_percentage.sh

--- scripts/coverage_percentage.sh
+++ scripts/coverage_percentage.sh
@@ -2,7 +2,7 @@
 percent=
 if [ -e /home/runner/work/portal-core/portal-core/target/site/jacoco/jacoco.csv ]
 then
-    percent=`awk -F, '/portal-core/{sumIM+= $4; sumIC+=$5} END {print sumIC/(sumIM+sumIC)*100}' /home/runner/work/portal-core/portal-core/target/site/jacoco/jacoco.csv`
+    percent=`awk -F, '/Portal-Core/{sumIM+= $4; sumIC+=$5} END {print sumIC/(sumIM+sumIC)*100}' /home/runner/work/portal-core/portal-core/target/site/jacoco/jacoco.csv`
 else
     percent="coverage file [jacoco.csv] not found"
 fi
