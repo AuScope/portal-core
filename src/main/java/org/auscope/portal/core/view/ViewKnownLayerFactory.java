f8456fd8ca8b72d164ed21095750767b027aa525
stuart.woodman <stuart.woodman@csiro.au>
2023-03-01 14:06:49 +1100

    Replaced KnownLayer's staticLegendUrl field with legendImg.

--- src/main/java/org/auscope/portal/core/view/ViewKnownLayerFactory.java
+++ src/main/java/org/auscope/portal/core/view/ViewKnownLayerFactory.java
@@ -50,7 +50,7 @@ public class ViewKnownLayerFactory {
         obj.put("feature_count", k.getFeature_count());
         obj.put("order", k.getOrder());
         obj.put("singleTile", k.getSingleTile());
-        obj.put("staticLegendUrl", k.getStaticLegendUrl());
+        obj.put("legendImg", k.getLegendImg());
 
         String group = "Others";
         if (k.getGroup() != null && !k.getGroup().isEmpty()) {
